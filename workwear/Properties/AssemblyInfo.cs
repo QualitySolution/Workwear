a10a4ba07a0b173675b80d9481ab11608d1988f1
Andrey Gankov <gav@qsolution.ru>
2014-05-26 17:08:09 +0400

    Подготовка к релизу 1.0

--- workwear/Properties/AssemblyInfo.cs
+++ workwear/Properties/AssemblyInfo.cs
@@ -3,18 +3,18 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle("workwear")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("QS: Спецодежда и имущество")]
+[assembly: AssemblyDescription("Программа позволяет вести учет внутреннего скалада спецодежды и имущества выдаваемого на объекты.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Quality Solution")]
+[assembly: AssemblyCompany("Quality Solution")]
+[assembly: AssemblyProduct("QS: Спецодежда и имущество")]
+[assembly: AssemblyCopyright("Quality Solution 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
