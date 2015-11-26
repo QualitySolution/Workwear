91b4db83bd0583cb5435837c7b04529556fc06d9
Andrey Gankov <gav@qsolution.ru>
2015-11-26 12:23:03 +0300

    Поиск по справочникам.

--- workwear/CreateProjectParam.cs
+++ workwear/CreateProjectParam.cs
@@ -23,12 +23,12 @@ namespace workwear
 			});
 			OrmMain.ClassMappingList = new System.Collections.Generic.List<IOrmObjectMapping> {
 				OrmObjectMapping<Norm>.Create ().Dialog<NormDlg> (),
-				OrmObjectMapping<ItemsType>.Create ().Dialog<ItemTypeDlg> ().SimpleDisplay ().Column ("Наименование", i => i.Name).End (),
-				OrmObjectMapping<Facility>.Create ().Dialog<ObjectDlg> ().SimpleDisplay ().Column ("Название", e => e.Name).Column ("Адрес", e => e.Address).End (),
-				OrmObjectMapping<Post>.Create ().SimpleDisplay ().Column ("Название", e => e.Name).End (),
-				OrmObjectMapping<Leader>.Create ().SimpleDisplay ().Column ("Имя", e => e.Name).End (),
+				OrmObjectMapping<ItemsType>.Create ().Dialog<ItemTypeDlg> ().SimpleDisplay ().SearchColumn ("Наименование", i => i.Name).End (),
+				OrmObjectMapping<Facility>.Create ().Dialog<ObjectDlg> ().SimpleDisplay ().SearchColumn ("Название", e => e.Name).SearchColumn ("Адрес", e => e.Address).End (),
+				OrmObjectMapping<Post>.Create ().SimpleDisplay ().SearchColumn ("Название", e => e.Name).End (),
+				OrmObjectMapping<Leader>.Create ().SimpleDisplay ().SearchColumn ("Имя", e => e.Name).End (),
 				OrmObjectMapping<User>.Create ().SimpleDisplay ().Column ("Имя", e => e.Name).End (),
-				OrmObjectMapping<EmployeeCard>.Create ().Dialog<EmployeeCardDlg>().SimpleDisplay ().Column ("Имя", e => e.FullName).End ()
+				OrmObjectMapping<EmployeeCard>.Create ().Dialog<EmployeeCardDlg>().SimpleDisplay ().SearchColumn ("Имя", e => e.FullName).End ()
 			};
 		}
 	}
