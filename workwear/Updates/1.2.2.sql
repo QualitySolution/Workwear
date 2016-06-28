914512400800082e7c74e4d825eee770e15b2590
Andrey Gankov <gav@qsolution.ru>
2016-06-28 18:16:00 +0300

    Убрал из скрипта микроапдейта название базы.

--- workwear/Updates/1.2.2.sql
+++ workwear/Updates/1.2.2.sql
@@ -1,2 +1,2 @@
-﻿ALTER TABLE `workwear_depot`.`item_types` 
+﻿ALTER TABLE `item_types` 
 CHANGE COLUMN `wear_category` `wear_category` ENUM('Wear', 'Shoes', 'Headgear', 'Gloves', 'PPE') NULL DEFAULT NULL ;
