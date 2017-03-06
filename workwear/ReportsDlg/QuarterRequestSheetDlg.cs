50133faa2481300729877e908259f439ff811711
Andrey Gankov <gav@qsolution.ru>
2017-03-06 15:41:47 +0300

    Добавлено формирование месячной заявки.

--- workwear/ReportsDlg/QuarterRequestSheetDlg.cs
+++ workwear/ReportsDlg/QuarterRequestSheetDlg.cs
@@ -9,22 +9,72 @@ namespace workwear
 		public QuarterRequestSheetDlg ()
 		{
 			this.Build ();
-			var list = new List<Quarter> ();
-			var quarter = new Quarter ((DateTime.Today.Month + 2) / 3, DateTime.Today.Year);
-			for(int i = 0; i < 5; i++)
-			{
-				list.Add (quarter);
-				quarter = quarter.GetNext ();
-			}
-			comboQuarter.ItemsList = list;
-			comboQuarter.SelectedItem = list [1];
+			SwitchDialog(PeriodType.Month);
 		}
 
 		protected void OnButtonOkClicked (object sender, EventArgs e)
 		{
 			var quart = comboQuarter.SelectedItem as Quarter;
-			string param = String.Format("quarter={0}&year={1}", quart.Number, quart.Year) ;
-			ViewReportExt.Run("QuarterRequestSheet", param);
+			if(quart != null)
+			{
+				string param = String.Format("quarter={0}&year={1}", quart.Number, quart.Year) ;
+				ViewReportExt.Run("QuarterRequestSheet", param);
+				return;
+			}
+			var month = comboQuarter.SelectedItem as Month;
+			if(month != null)
+			{
+				string param = String.Format("month={0}&year={1}", month.Number, month.Year) ;
+				ViewReportExt.Run("MonthRequestSheet", param);
+				return;
+			}
+		}
+
+		private void SwitchDialog(PeriodType type)
+		{
+			switch (type)
+			{
+				case PeriodType.Quarter:
+					var list = new List<Quarter> ();
+					var quarter = new Quarter ((DateTime.Today.Month + 2) / 3, DateTime.Today.Year);
+					for(int i = 0; i < 5; i++)
+					{
+						list.Add (quarter);
+						quarter = quarter.GetNext ();
+					}
+					comboQuarter.ItemsList = list;
+					comboQuarter.SelectedItem = list [1];
+					break;
+
+				case PeriodType.Month:
+					var listM = new List<Month> ();
+					var month = new Month (DateTime.Today.AddMonths(-1));
+					for(int i = 0; i < 14; i++)
+					{
+						listM.Add (month);
+						month = month.GetNext ();
+					}
+					comboQuarter.ItemsList = listM;
+					comboQuarter.SelectedItem = listM [2];
+					break;
+			}
+		}
+
+		protected void OnRadioMonthToggled(object sender, EventArgs e)
+		{
+			if (radioMonth.Active)
+				SwitchDialog(PeriodType.Month);
+		}
+
+		protected void OnRadioQuarterToggled(object sender, EventArgs e)
+		{
+			if (radioQuarter.Active)
+				SwitchDialog(PeriodType.Quarter);
+		}
+
+		enum PeriodType{
+			Month,
+			Quarter
 		}
 	}
 
@@ -49,5 +99,34 @@ namespace workwear
 			return newNum == 5 ? new Quarter (1, Year + 1) : new Quarter (newNum, Year);
 		}
 	}
+
+	public class Month{
+		public int Number;
+		public int Year;
+
+		public string Title{
+			get{ return String.Format ("{0:MMMM yyyy}", new DateTime(Year, Number, 1));
+			}
+		}
+
+		public Month(int num, int year)
+		{
+			Number = num;
+			Year = year;
+		}
+
+		public Month(DateTime date)
+		{
+			Number = date.Month;
+			Year = date.Year;
+		}
+
+		public Month GetNext()
+		{
+			int newNum = Number + 1;
+			return newNum == 13 ? new Month (1, Year + 1) : new Month (newNum, Year);
+		}
+	}
+
 }
 
--- workwear/gtk-gui/workwear.QuarterRequestSheetDlg.cs
+++ workwear/gtk-gui/workwear.QuarterRequestSheetDlg.cs
@@ -8,7 +8,15 @@ namespace workwear
 		
 		private global::Gamma.Widgets.ySpecComboBox comboQuarter;
 		
-		private global::Gtk.Label label1;
+		private global::Gtk.HBox hbox1;
+		
+		private global::Gtk.RadioButton radioMonth;
+		
+		private global::Gtk.RadioButton radioQuarter;
+		
+		private global::Gtk.Label label2;
+		
+		private global::Gtk.Label labelPeriodType;
 		
 		private global::Gtk.Button buttonCancel;
 		
@@ -19,13 +27,14 @@ namespace workwear
 			global::Stetic.Gui.Initialize (this);
 			// Widget workwear.QuarterRequestSheetDlg
 			this.Name = "workwear.QuarterRequestSheetDlg";
+			this.Title = global::Mono.Unix.Catalog.GetString ("Формирование заявки");
 			this.WindowPosition = ((global::Gtk.WindowPosition)(4));
 			// Internal child workwear.QuarterRequestSheetDlg.VBox
 			global::Gtk.VBox w1 = this.VBox;
 			w1.Name = "dialog1_VBox";
 			w1.BorderWidth = ((uint)(2));
 			// Container child dialog1_VBox.Gtk.Box+BoxChild
-			this.table1 = new global::Gtk.Table (((uint)(1)), ((uint)(2)), false);
+			this.table1 = new global::Gtk.Table (((uint)(2)), ((uint)(2)), false);
 			this.table1.Name = "table1";
 			this.table1.RowSpacing = ((uint)(6));
 			this.table1.ColumnSpacing = ((uint)(6));
@@ -34,32 +43,75 @@ namespace workwear
 			this.comboQuarter = new global::Gamma.Widgets.ySpecComboBox ();
 			this.comboQuarter.Name = "comboQuarter";
 			this.comboQuarter.AddIfNotExist = false;
+			this.comboQuarter.DefaultFirst = false;
 			this.comboQuarter.ShowSpecialStateAll = false;
 			this.comboQuarter.ShowSpecialStateNot = false;
 			this.table1.Add (this.comboQuarter);
 			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table1 [this.comboQuarter]));
+			w2.TopAttach = ((uint)(1));
+			w2.BottomAttach = ((uint)(2));
 			w2.LeftAttach = ((uint)(1));
 			w2.RightAttach = ((uint)(2));
 			w2.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table1.Gtk.Table+TableChild
-			this.label1 = new global::Gtk.Label ();
-			this.label1.Name = "label1";
-			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("Период:");
-			this.table1.Add (this.label1);
-			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table1 [this.label1]));
-			w3.XOptions = ((global::Gtk.AttachOptions)(4));
-			w3.YOptions = ((global::Gtk.AttachOptions)(4));
+			this.hbox1 = new global::Gtk.HBox ();
+			this.hbox1.Name = "hbox1";
+			this.hbox1.Spacing = 6;
+			// Container child hbox1.Gtk.Box+BoxChild
+			this.radioMonth = new global::Gtk.RadioButton (global::Mono.Unix.Catalog.GetString ("Месяц"));
+			this.radioMonth.CanFocus = true;
+			this.radioMonth.Name = "radioMonth";
+			this.radioMonth.DrawIndicator = true;
+			this.radioMonth.UseUnderline = true;
+			this.radioMonth.Group = new global::GLib.SList (global::System.IntPtr.Zero);
+			this.hbox1.Add (this.radioMonth);
+			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.radioMonth]));
+			w3.Position = 0;
+			// Container child hbox1.Gtk.Box+BoxChild
+			this.radioQuarter = new global::Gtk.RadioButton (global::Mono.Unix.Catalog.GetString ("Квартал"));
+			this.radioQuarter.CanFocus = true;
+			this.radioQuarter.Name = "radioQuarter";
+			this.radioQuarter.DrawIndicator = true;
+			this.radioQuarter.UseUnderline = true;
+			this.radioQuarter.Group = this.radioMonth.Group;
+			this.hbox1.Add (this.radioQuarter);
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.radioQuarter]));
+			w4.Position = 1;
+			this.table1.Add (this.hbox1);
+			global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.table1 [this.hbox1]));
+			w5.LeftAttach = ((uint)(1));
+			w5.RightAttach = ((uint)(2));
+			w5.XOptions = ((global::Gtk.AttachOptions)(4));
+			w5.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table1.Gtk.Table+TableChild
+			this.label2 = new global::Gtk.Label ();
+			this.label2.Name = "label2";
+			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("Период:");
+			this.table1.Add (this.label2);
+			global::Gtk.Table.TableChild w6 = ((global::Gtk.Table.TableChild)(this.table1 [this.label2]));
+			w6.XOptions = ((global::Gtk.AttachOptions)(4));
+			w6.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table1.Gtk.Table+TableChild
+			this.labelPeriodType = new global::Gtk.Label ();
+			this.labelPeriodType.Name = "labelPeriodType";
+			this.labelPeriodType.LabelProp = global::Mono.Unix.Catalog.GetString ("Квартал:");
+			this.table1.Add (this.labelPeriodType);
+			global::Gtk.Table.TableChild w7 = ((global::Gtk.Table.TableChild)(this.table1 [this.labelPeriodType]));
+			w7.TopAttach = ((uint)(1));
+			w7.BottomAttach = ((uint)(2));
+			w7.XOptions = ((global::Gtk.AttachOptions)(4));
+			w7.YOptions = ((global::Gtk.AttachOptions)(4));
 			w1.Add (this.table1);
-			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(w1 [this.table1]));
-			w4.Position = 0;
-			w4.Expand = false;
-			w4.Fill = false;
+			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(w1 [this.table1]));
+			w8.Position = 0;
+			w8.Expand = false;
+			w8.Fill = false;
 			// Internal child workwear.QuarterRequestSheetDlg.ActionArea
-			global::Gtk.HButtonBox w5 = this.ActionArea;
-			w5.Name = "dialog1_ActionArea";
-			w5.Spacing = 10;
-			w5.BorderWidth = ((uint)(5));
-			w5.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
+			global::Gtk.HButtonBox w9 = this.ActionArea;
+			w9.Name = "dialog1_ActionArea";
+			w9.Spacing = 10;
+			w9.BorderWidth = ((uint)(5));
+			w9.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonCancel = new global::Gtk.Button ();
 			this.buttonCancel.CanDefault = true;
@@ -69,9 +121,9 @@ namespace workwear
 			this.buttonCancel.UseUnderline = true;
 			this.buttonCancel.Label = "gtk-cancel";
 			this.AddActionWidget (this.buttonCancel, -6);
-			global::Gtk.ButtonBox.ButtonBoxChild w6 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w5 [this.buttonCancel]));
-			w6.Expand = false;
-			w6.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w10 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonCancel]));
+			w10.Expand = false;
+			w10.Fill = false;
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonOk = new global::Gtk.Button ();
 			this.buttonOk.CanDefault = true;
@@ -81,16 +133,18 @@ namespace workwear
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = "gtk-ok";
 			this.AddActionWidget (this.buttonOk, -5);
-			global::Gtk.ButtonBox.ButtonBoxChild w7 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w5 [this.buttonOk]));
-			w7.Position = 1;
-			w7.Expand = false;
-			w7.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w11 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonOk]));
+			w11.Position = 1;
+			w11.Expand = false;
+			w11.Fill = false;
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
-			this.DefaultWidth = 294;
-			this.DefaultHeight = 153;
+			this.DefaultWidth = 290;
+			this.DefaultHeight = 186;
 			this.Show ();
+			this.radioMonth.Toggled += new global::System.EventHandler (this.OnRadioMonthToggled);
+			this.radioQuarter.Toggled += new global::System.EventHandler (this.OnRadioQuarterToggled);
 			this.buttonOk.Clicked += new global::System.EventHandler (this.OnButtonOkClicked);
 		}
 	}
