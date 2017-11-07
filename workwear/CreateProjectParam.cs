c0b59112f776aeec196d9d1792e0480ba6f2d0cc
Andrey Gankov <gav@qsolution.ru>
2017-11-07 11:42:25 +0300

    Переписал маппинг на настройку через вызовы отдельных методов. Так удобнее в дальнейшем будет работать.

--- workwear/CreateProjectParam.cs
+++ workwear/CreateProjectParam.cs
@@ -25,24 +25,26 @@ namespace workwear
 				System.Reflection.Assembly.GetAssembly (typeof(MeasurementUnits)),
 			});
 
-			OrmMain.ClassMappingList = new System.Collections.Generic.List<IOrmObjectMapping> {
-				MeasurementUnits.GetOrmMapping (),
-				OrmObjectMapping<Norm>.Create ().Dialog<NormDlg> (),
-				OrmObjectMapping<ItemsType>.Create ().Dialog<ItemTypeDlg> ().DefaultTableView ().SearchColumn ("Наименование", i => i.Name).OrderAsc (i => i.Name).End (),
-				OrmObjectMapping<Facility>.Create ().Dialog<ObjectDlg> ().DefaultTableView ().SearchColumn ("Название", e => e.Name).SearchColumn ("Адрес", e => e.Address).End (),
-				OrmObjectMapping<Post>.Create ().DefaultTableView ().SearchColumn ("Название", e => e.Name).End (),
-				OrmObjectMapping<Leader>.Create ().DefaultTableView ().SearchColumn ("Имя", e => e.Name).End (),
-				OrmObjectMapping<User>.Create ().DefaultTableView ().Column ("Имя", e => e.Name).End (),
-				OrmObjectMapping<EmployeeCard>.Create ().Dialog<EmployeeCardDlg>().UseSlider(false).DefaultTableView ().SearchColumn ("Имя", e => e.FullName).End (),
-				//Склад
-				OrmObjectMapping<Nomenclature>.Create ().Dialog<NomenclatureDlg> ().DefaultTableView ()
+			//Настраиваем классы сущьностей
+
+			OrmMain.AddObjectDescription(MeasurementUnits.GetOrmMapping());
+			//Спецодежда
+			OrmMain.AddObjectDescription<Norm>().Dialog<NormDlg>();
+			OrmMain.AddObjectDescription<ItemsType>().Dialog<ItemTypeDlg> ().DefaultTableView ().SearchColumn ("Наименование", i => i.Name).OrderAsc (i => i.Name).End ();
+			OrmMain.AddObjectDescription<Facility>().Dialog<ObjectDlg> ().DefaultTableView ().SearchColumn ("Название", e => e.Name).SearchColumn ("Адрес", e => e.Address).End ();
+			OrmMain.AddObjectDescription<Post>().DefaultTableView ().SearchColumn ("Название", e => e.Name).End ();
+			OrmMain.AddObjectDescription<Leader>().DefaultTableView ().SearchColumn ("Имя", e => e.Name).End ();
+			OrmMain.AddObjectDescription<EmployeeCard>().Dialog<EmployeeCardDlg>().UseSlider(false).DefaultTableView ().SearchColumn ("Имя", e => e.FullName).End ();
+			//Общее
+			OrmMain.AddObjectDescription<User>().DefaultTableView ().Column ("Имя", e => e.Name).End ();
+			//Склад
+			OrmMain.AddObjectDescription<Nomenclature>().Dialog<NomenclatureDlg> ().DefaultTableView ()
 					.SearchColumn ("Наименование", i => i.Name)
 					.SearchColumn ("Размер", i => i.Size)
-					.SearchColumn ("Рост", i => i.WearGrowth).OrderAsc (i => i.Name).End (),
-				OrmObjectMapping<Income>.Create().Dialog<IncomeDocDlg>(),
-				OrmObjectMapping<Expense>.Create().Dialog<ExpenseDocDlg>(),
-				OrmObjectMapping<Writeoff>.Create().Dialog<WriteOffDocDlg>(),
-			};
+					.SearchColumn ("Рост", i => i.WearGrowth).OrderAsc (i => i.Name).End ();
+			OrmMain.AddObjectDescription<Income>().Dialog<IncomeDocDlg>();
+			OrmMain.AddObjectDescription<Expense>().Dialog<ExpenseDocDlg>();
+			OrmMain.AddObjectDescription<Writeoff>().Dialog<WriteOffDocDlg>();
 		}
 	}
 }
