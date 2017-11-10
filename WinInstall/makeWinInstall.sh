db7fdc21dd2c79091fb2e45929218a7ba7eccbff
Andrey Gankov <gav@qsolution.ru>
2017-11-10 12:23:50 +0300

    Скрипт сборки сетапа теперь сам собирает экзешник для винды.

--- WinInstall/makeWinInstall.sh
+++ WinInstall/makeWinInstall.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+set -e
 
 ProjectName="workwear"
-BinDir=../$ProjectName/bin/Release
+BinDir=../$ProjectName/bin/ReleaseWin
+
+# Сборка релиза
+msbuild /p:Configuration=ReleaseWin /p:Platform=x86 ../workwear.sln
 
 # Очистка бин от лишний файлов
 
 rm -v ${BinDir}/*.mdb
 rm -v ${BinDir}/*.pdb
+rm -v -R ./Files/*
 
 cp -r -v ${BinDir}/* ./Files
 
