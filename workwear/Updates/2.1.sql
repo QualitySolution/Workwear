9d3a1a85da1052e6e17caa999e978804637c3b5b
Andrey Gankov <gav@qsolution.ru>
2018-11-28 11:19:23 +0300

    Добавлено заполнение полей сроков носки и даты начала по старым данным.

--- workwear/Updates/2.1.sql
+++ workwear/Updates/2.1.sql
@@ -4,9 +4,11 @@
 ALTER TABLE operation_issued_by_employee ADD COLUMN temp_id INT(10) UNSIGNED;
 
 #Выдача сотрудникам
-INSERT INTO `operation_issued_by_employee`(`employee_id`, `operation_time`, `nomenclature_id`, `wear_percent`, `issued`, `returned`, `auto_writeoff_date`, `issued_operation_id`, `stock_income_detail_id`, `buh_document`, `temp_id`) 
+INSERT INTO `operation_issued_by_employee`(`employee_id`, `operation_time`, `nomenclature_id`, `wear_percent`, `issued`, `returned`, 
+`auto_writeoff_date`, `issued_operation_id`, `stock_income_detail_id`, `buh_document`, `temp_id`, `StartOfUse`, `ExpenseByNorm`) 
 SELECT stock_expense.wear_card_id, stock_expense.date, stock_expense_detail.nomenclature_id, 
-1.0 - stock_income_detail.life_percent, stock_expense_detail.quantity, 0, stock_expense_detail.auto_writeoff_date, NULL, stock_expense_detail.stock_income_detail_id, stock_expense.comment, stock_expense_detail.id
+1.0 - stock_income_detail.life_percent, stock_expense_detail.quantity, 0, stock_expense_detail.auto_writeoff_date, 
+NULL, stock_expense_detail.stock_income_detail_id, stock_expense.comment, stock_expense_detail.id, stock_expense.date, stock_expense_detail.auto_writeoff_date
 FROM stock_expense_detail
 LEFT JOIN stock_expense ON stock_expense.id = stock_expense_detail.stock_expense_id
 LEFT JOIN stock_income_detail ON stock_expense_detail.stock_income_detail_id = stock_income_detail.id
