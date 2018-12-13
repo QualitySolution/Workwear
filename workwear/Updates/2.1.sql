6564d4388cce632ac03f2ee6618e2f31d6502c85
Andrey Gankov <gav@qsolution.ru>
2018-12-13 09:23:03 +0300

    Исправлен скрипт обновления базы до 2.1. В него случайно попало часть изменение уже включенных в 2.0.2

--- workwear/Updates/2.1.sql
+++ workwear/Updates/2.1.sql
@@ -1,10 +1,4 @@
-﻿
-ALTER TABLE `wear_cards` 
-ADD COLUMN `change_of_position_date` DATE NULL DEFAULT NULL AFTER `hire_date`;
-
-ALTER TABLE `stock_income_detail` 
-CHANGE COLUMN `life_percent` `life_percent` DECIMAL(3,2) UNSIGNED NOT NULL DEFAULT 1 ,
-ADD COLUMN `certificate` VARCHAR(40) NULL DEFAULT NULL AFTER `cost`,
+﻿ALTER TABLE `stock_income_detail` 
 ADD COLUMN `employee_issue_operation_id` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `certificate`,
 ADD INDEX `fk_stock_income_detail_1_idx` (`employee_issue_operation_id` ASC);
 
@@ -15,38 +9,6 @@ ADD INDEX `fk_stock_expense_detail_1_idx` (`employee_issue_operation_id` ASC);
 ALTER TABLE `stock_write_off_detail` 
 ADD COLUMN `employee_issue_operation_id` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `quantity`,
 ADD INDEX `fk_stock_write_off_detail_1_idx` (`employee_issue_operation_id` ASC);
-
-ALTER TABLE `norms` 
-ADD COLUMN `regulations_id` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `id`,
-ADD COLUMN `regulations_annex_id` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `regulations_id`,
-ADD INDEX `fk_norms_1_idx` (`regulations_id` ASC),
-ADD INDEX `fk_norms_2_idx` (`regulations_annex_id` ASC);
-
-CREATE TABLE IF NOT EXISTS `regulations` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` TINYTEXT NOT NULL,
-  `number` VARCHAR(10) NULL DEFAULT NULL,
-  `doc_date` DATE NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1000
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `regulations_annex` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `regulations_id` INT(10) UNSIGNED NOT NULL,
-  `number` TINYINT(4) NOT NULL,
-  `name` TINYTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_regulations_appendix_1_idx` (`regulations_id` ASC),
-  CONSTRAINT `fk_regulations_appendix_1`
-    FOREIGN KEY (`regulations_id`)
-    REFERENCES `regulations` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 10000
-DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `operation_issued_by_employee` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -119,19 +81,7 @@ ADD CONSTRAINT `fk_stock_write_off_detail_1`
   REFERENCES `operation_issued_by_employee` (`id`)
   ON DELETE RESTRICT
   ON UPDATE CASCADE;
-
-ALTER TABLE `norms` 
-ADD CONSTRAINT `fk_norms_1`
-  FOREIGN KEY (`regulations_id`)
-  REFERENCES `regulations` (`id`)
-  ON DELETE RESTRICT
-  ON UPDATE CASCADE,
-ADD CONSTRAINT `fk_norms_2`
-  FOREIGN KEY (`regulations_annex_id`)
-  REFERENCES `regulations_annex` (`id`)
-  ON DELETE RESTRICT
-  ON UPDATE CASCADE;
-
+  
 #Миграция данных
 
 ALTER TABLE operation_issued_by_employee ADD COLUMN temp_id INT(10) UNSIGNED;
