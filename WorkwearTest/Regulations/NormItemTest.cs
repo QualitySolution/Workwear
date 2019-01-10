22ad61566d784e170eaf0c8aacc78f2a6322ab92
Andrey Gankov <gav@qsolution.ru>
2019-01-10 18:31:06 +0300

    Добавил в тест проверку конкретного случая на который жаловалась Сабина.

--- WorkwearTest/Regulations/NormItemTest.cs
+++ WorkwearTest/Regulations/NormItemTest.cs
@@ -23,6 +23,7 @@ namespace WorkwearTest.Regulations
 		[Test]
 		[TestCase(2, NormPeriodType.Year, 2, "2018-01-01", 3, ExpectedResult = "2021-01-01")]
 		[TestCase(1, NormPeriodType.Month, 3, "2018-01-10", 2, ExpectedResult = "2018-07-10")]
+		[TestCase(1, NormPeriodType.Year, 2, "2018-10-19", 1, ExpectedResult = "2020-10-19")]
 		public DateTime CalculateExpireDateProportionalTest(int normAmount, NormPeriodType normPeriod, int periodCount, DateTime startdate, int amount)
 		{
 			var item = new NormItem();
