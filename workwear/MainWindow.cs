e4d84715a05d3ce35e15b382864d8eb21510e4c5
Andrey Gankov <gav@qsolution.ru>
2019-05-20 14:53:35 +0300

    Добавлена работа буфера обмена Ctrl+V и прочее, Добавлена возможность сохранять диалог Ctrl+S

--- workwear/MainWindow.cs
+++ workwear/MainWindow.cs
@@ -27,7 +27,6 @@ public partial class MainWindow : Gtk.Window
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
 	{
 		Build();
-
 		//Передаем лебл
 		QSMain.StatusBarLabel = labelStatus;
 		this.Title = MainSupport.GetTitle();
@@ -69,6 +68,9 @@ public partial class MainWindow : Gtk.Window
 			md.Destroy();
 			dialogAuthenticationAction.Sensitive = false;
 		}
+
+		this.KeyReleaseEvent += ClipboardWorkaround.HandleKeyReleaseEvent;
+		this.KeyReleaseEvent += QS.Tdi.Gtk.TDIMain.TDIHandleKeyReleaseEvent;
 
 		UsersAction.Sensitive = QSMain.User.Admin;
 		labelUser.LabelProp = QSMain.User.Name;
