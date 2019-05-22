8846a5fc4fbc59b8f64e61fe84ec52c2777d5c3e
Andrey Gankov <gav@qsolution.ru>
2019-05-22 09:08:02 +0300

    Исправлена сохранение отчетов в эксель, в сборку не передавался файл SharpZipLib. Так же написаны тесты для проверки что это не поломалось.

---
+++ WorkwearTest/Integration/ReportExportTest.cs
@@ -0,0 +1,31 @@
+﻿using fyiReporting.RDL;
+using fyiReporting.RdlGtkViewer;
+using NUnit.Framework;
+using System;
+using System.IO;
+using QS.Utilities.Text;
+
+namespace WorkwearTest.Integration
+{
+	[TestFixture()]
+	public class ReportExportTest
+	{
+		[Test(Description = "Проверяем что можем сделать экспорт Excel2007. Реальный кейс: отсутстивие ICSharpCode.SharpZipLib")]
+		public void Reports_ExportExcel2007_SharpZipLibExistInOutputDirTest()
+		{
+			var path = Directory.GetCurrentDirectory();
+			var filePath = Path.Combine(path.ReplaceLastOccurrence("WorkwearTest", "workwear"), "ICSharpCode.SharpZipLib.dll");
+			FileAssert.Exists(filePath);
+		}
+
+		[Test(Description = "Проверяем что можем сделать экспорт Excel2007. Проверяем работает ли NPOI.")]
+		public void Reports_ExportExcel2007Test()
+		{
+			var reportPath = Path.Combine(Directory.GetCurrentDirectory(), "Integration", "Reports", "NoDBTestReport.rdl");
+			using(MemoryStream stream = ReportExporter.ExportToMemoryStream(new Uri(reportPath), OutputPresentationType.Excel2007)) {
+				var file = stream.GetBuffer();
+				Assert.That(file.Length, Is.GreaterThan(1), "Размер файла должен быть больше 1 байта.");
+			}
+		}
+	}
+}
