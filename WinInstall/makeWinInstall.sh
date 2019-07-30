a2f28a0f3b1620ae6f0fa29bc4c076d2f94e240f
Gankov <gav@qsolution.ru>
2019-07-30 09:53:40 +0300

    При создании установщика, создаем каталог Files если его нет.

--- WinInstall/makeWinInstall.sh
+++ WinInstall/makeWinInstall.sh
@@ -15,6 +15,7 @@ rm -v -f ${BinDir}/*.mdb
 rm -v -f ${BinDir}/*.pdb
 rm -v -f -R ./Files/*
 
+mkdir -p Files
 cp -r -v ${BinDir}/* ./Files
 
 wine ~/.wine/drive_c/Program\ Files\ \(x86\)/NSIS/makensis.exe /INPUTCHARSET UTF8 ${ProjectName}.nsi
