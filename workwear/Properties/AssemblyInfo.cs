c362f1c6f2743d5ac60a485aef89606ca327d90f
Gankov <gav@qsolution.ru>
2020-01-23 11:53:45 +0300

    2.2.1

--- workwear/Properties/AssemblyInfo.cs
+++ workwear/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using QSProjectsLib;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: Спецодежда и имущество")]
-[assembly: AssemblyCopyright("Quality Solution 2019")]
+[assembly: AssemblyCopyright("Quality Solution 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyLogoIcon("workwear.icon.logo.png")]
@@ -27,7 +27,7 @@ using QSProjectsLib;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("2.2.0.4")]
+[assembly: AssemblyVersion("2.2.1")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
