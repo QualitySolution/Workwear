78a698cfe5ca52879fb8c786c11a7fad8d65be29
Mukim <mukim.fozilov@yandex.ru>
2020-02-05 13:43:29 +0300

    Таблица операций.

---
+++ Workwear/HibernateMapping/Stock/WarehouseOperationMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using workwear.Domain.Stock;
+
+namespace workwear.HibernateMapping.Stock
+{
+	public class WarehouseOperationMap : ClassMap<WarehouseOperation>
+	{
+		public WarehouseOperationMap()
+		{
+
+			Table("operation_warehouse");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+			Map(x => x.WareOperationTime).Column("operation_time");
+			Map(x => x.Size).Column("size");
+			Map(x => x.Growth).Column("growth");
+			Map(x => x.Amount).Column("amount");
+
+			References(x => x.ReceiptWarehouse).Column("warehouse_receipt_id");
+			References(x => x.ExpenseWarehouse).Column("warehouse_expense_id");
+			References(x => x.Nomenclature).Column("nomenclature_id").Not.Nullable();
+
+		}
+	}
+}
---
+++ Workwear/Views/Stock/WarehouseOperationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace workwear.Views.Stock
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class WarehouseOperationView : Gtk.Bin
+	{
+		public WarehouseOperationView()
+		{
+			this.Build();
+		}
+	}
+}
