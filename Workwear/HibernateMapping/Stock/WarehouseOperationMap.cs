1d51a85bbf2a93843f31b325d22d5c2a329da3e4
Mukim <mukim.fozilov@yandex.ru>
2020-02-12 11:51:04 +0300

    WarehouseOperation(DomainModelUpdate)

--- Workwear/HibernateMapping/Stock/WarehouseOperationMap.cs
+++ Workwear/HibernateMapping/Stock/WarehouseOperationMap.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using FluentNHibernate.Mapping;
-using workwear.Domain.Stock;
+using workwear.Domain.Operations;
 
 namespace workwear.HibernateMapping.Stock
 {
@@ -16,10 +16,13 @@ namespace workwear.HibernateMapping.Stock
 			Map(x => x.Size).Column("size");
 			Map(x => x.Growth).Column("growth");
 			Map(x => x.Amount).Column("amount");
+			Map(x => x.WearPercent).Column("wear_percent");
 
 			References(x => x.ReceiptWarehouse).Column("warehouse_receipt_id");
 			References(x => x.ExpenseWarehouse).Column("warehouse_expense_id");
 			References(x => x.Nomenclature).Column("nomenclature_id").Not.Nullable();
+			References(x => x.IncomeWarehouseOperation).Column("income_warehouse_operation_id");
+			References(x => x.EmployeeExpense).Column("employee_expense_id");
 
 		}
 	}
