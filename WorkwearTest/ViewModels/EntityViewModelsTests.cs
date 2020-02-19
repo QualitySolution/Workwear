bdfd37c0981469dd34ac27187ace50c3f43bdff7
Gankov <gav@qsolution.ru>
2020-02-19 13:02:03 +0300

    Тестирование моделей на добавление в конструктор валидатора.

---
+++ WorkwearTest/ViewModels/EntityViewModelsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using NUnit.Framework;
+using QS.ViewModels;
+using workwear.ViewModels.Company;
+
+namespace WorkwearTest.ViewModels
+{
+	[TestFixture]
+	[Category("Проверка ViewModel-ей")]
+	public class EntityViewModelsTests : EntityViewModelsTestsBase
+	{
+		static EntityViewModelsTests()
+		{
+			ScanAssemblies = new[] { Assembly.GetAssembly(typeof(OrganizationViewModel))};
+		}
+
+		public new static IEnumerable AllEntityViewModels => EntityViewModelsTestsBase.AllEntityViewModels;
+
+		[Test, TestCaseSource(nameof(AllEntityViewModels))]
+		public override void ViewModelForValidateblyEntityHasValidatorDependenceTest(Type type)
+		{
+			base.ViewModelForValidateblyEntityHasValidatorDependenceTest(type);
+		}
+	}
+}
