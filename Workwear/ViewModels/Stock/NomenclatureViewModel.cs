a3515c1ebae0a01a9bed2ea7ae8b8e5ca1c4dbd0
Gankov <gav@qsolution.ru>
2020-03-02 00:02:05 +0300

    Удалил мусор и пометил заглушки заглушками.

---
+++ Workwear/ViewModels/Stock/NomenclatureViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Validation;
+using QS.ViewModels.Dialog;
+using workwear.Domain.Stock;
+
+namespace workwear.ViewModels.Stock
+{
+	public class NomenclatureViewModel : EntityDialogViewModelBase<Nomenclature>
+	{
+		public NomenclatureViewModel(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, INavigationManager navigation, IValidator validator = null) : base(uowBuilder, unitOfWorkFactory, navigation, validator)
+		{
+			//FIXME Заглушка пока не используется.
+		}
+	}
+}
