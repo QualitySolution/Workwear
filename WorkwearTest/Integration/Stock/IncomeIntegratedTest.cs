ac547a80c6387e5c8cc4694743e70c1e65b6c4bb
Gankov <gav@qsolution.ru>
2020-03-16 20:08:48 +0300

    Добавлен простой тест проверяющий приход на два разных склада.

--- WorkwearTest/Integration/Stock/IncomeIntegratedTest.cs
+++ WorkwearTest/Integration/Stock/IncomeIntegratedTest.cs
@@ -147,5 +147,61 @@ namespace WorkwearTest.Integration.Stock
 			}
 		}
 
+		#region Проверка влияния на складские остатки
+
+		[Test(Description = "Проверяем что на складе появляются отдельные остатки по двум складам.")]
+		[Category("Integrated")]
+		public void IncomeOnTwoWarehousesTest()
+		{
+			var ask = Substitute.For<IInteractiveQuestion>();
+			ask.Question(string.Empty).ReturnsForAnyArgs(true);
+
+			using(var uow = UnitOfWorkFactory.CreateWithoutRoot()) {
+				var warehouse = new Warehouse();
+				uow.Save(warehouse);
+
+				var warehouse2 = new Warehouse();
+				uow.Save(warehouse2);
+
+				var nomenclatureType = new ItemsType();
+				nomenclatureType.Name = "Тестовый тип номенклатуры";
+				uow.Save(nomenclatureType);
+
+				var nomenclature = new Nomenclature();
+				nomenclature.Type = nomenclatureType;
+				uow.Save(nomenclature);
+
+				var income1 = new Income();
+				income1.Warehouse = warehouse;
+				income1.Date = new DateTime(2017, 1, 1);
+				income1.Operation = IncomeOperations.Enter;
+				var incomeItem1 = income1.AddItem(nomenclature);
+				incomeItem1.Size = "X";
+				incomeItem1.Amount = 10;
+				var incomeItem2 = income1.AddItem(nomenclature);
+				incomeItem2.Size = "X";
+				incomeItem2.Amount = 5;
+				income1.UpdateOperations(uow, ask);
+				uow.Save(income1);
+
+				var income2 = new Income();
+				income2.Warehouse = warehouse2;
+				income2.Date = new DateTime(2017, 1, 1);
+				income2.Operation = IncomeOperations.Enter;
+				var income2Item1 = income2.AddItem(nomenclature);
+				income2Item1.Size = "X";
+				income2Item1.Amount = 7;
+				income2.UpdateOperations(uow, ask);
+				uow.Save(income2);
+
+				var stockRepository = new StockRepository();
+				var stock1 = stockRepository.StockBalances(uow, warehouse, new List<Nomenclature> { nomenclature }, new DateTime(2017, 1, 2));
+				Assert.That(stock1.Sum(x => x.Amount), Is.EqualTo(15));
+				var stock2 = stockRepository.StockBalances(uow, warehouse2, new List<Nomenclature> { nomenclature }, new DateTime(2017, 1, 2));
+				Assert.That(stock2.Sum(x => x.Amount), Is.EqualTo(7));
+			}
+		}
+
+		#endregion
 	}
 }
