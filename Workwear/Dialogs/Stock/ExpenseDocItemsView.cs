b21b01843e23aa7c29b50a2f2a01aecf5e1b1837
Gankov <gav@qsolution.ru>
2020-08-07 10:06:23 +0300

    Добавлена возможность открыть номеклатуру через контекстное меню.

--- Workwear/Dialogs/Stock/ExpenseDocItemsView.cs
+++ Workwear/Dialogs/Stock/ExpenseDocItemsView.cs
@@ -7,6 +7,7 @@ using Gtk;
 using NLog;
 using QS.Dialog.Gtk;
 using QSOrmProject;
+using QSWidgetLib;
 using workwear.Domain.Company;
 using workwear.Domain.Stock;
 
@@ -107,7 +108,31 @@ namespace workwear
 				.AddColumn("")
 				.Finish ();
 			ytreeItems.Selection.Changed += YtreeItems_Selection_Changed;
+			ytreeItems.ButtonReleaseEvent += YtreeItems_ButtonReleaseEvent;
 		}
+
+		#region PopupMenu
+		void YtreeItems_ButtonReleaseEvent(object o, ButtonReleaseEventArgs args)
+		{
+			if(args.Event.Button == 3) {
+				var menu = new Menu();
+				var selected = ytreeItems.GetSelectedObject<ExpenseItem>();
+				var item = new MenuItemId<ExpenseItem>("Открыть номеклатуру");
+				item.ID = selected;
+				item.Activated += Item_Activated;
+				menu.Add(item);
+				menu.ShowAll();
+				menu.Popup();
+			}
+		}
+
+		void Item_Activated(object sender, EventArgs e)
+		{
+			var item = (sender as MenuItemId<ExpenseItem>).ID;
+			OpenTab<NomenclatureDlg, int>(item.Nomenclature.Id);
+		}
+
+		#endregion
 
 		void YtreeItems_Selection_Changed (object sender, EventArgs e)
 		{
--- Workwear/Dialogs/Stock/WriteOffItemsView.cs
+++ Workwear/Dialogs/Stock/WriteOffItemsView.cs
@@ -8,6 +8,7 @@ using workwear.Domain.Operations;
 using workwear.Domain.Company;
 using workwear.Domain.Stock;
 using workwear.Representations.Organization;
+using QSWidgetLib;
 
 namespace workwear
 {
@@ -69,7 +70,31 @@ namespace workwear
 				.AddSetter((c, e) => c.Editable = e.IssuedOn?.ExpenseDoc.Operation == ExpenseOperations.Employee)
 				.Finish ();
 			ytreeItems.Selection.Changed += YtreeItems_Selection_Changed;
+			ytreeItems.ButtonReleaseEvent += YtreeItems_ButtonReleaseEvent;
 		}
+
+		#region PopupMenu
+		void YtreeItems_ButtonReleaseEvent(object o, ButtonReleaseEventArgs args)
+		{
+			if(args.Event.Button == 3) {
+				var menu = new Menu();
+				var selected = ytreeItems.GetSelectedObject<WriteoffItem>();
+				var item = new MenuItemId<WriteoffItem>("Открыть номеклатуру");
+				item.ID = selected;
+				item.Activated += Item_Activated;
+				menu.Add(item);
+				menu.ShowAll();
+				menu.Popup();
+			}
+		}
+
+		void Item_Activated(object sender, EventArgs e)
+		{
+			var item = (sender as MenuItemId<WriteoffItem>).ID;
+			OpenTab<NomenclatureDlg, int>(item.Nomenclature.Id);
+		}
+
+		#endregion
 
 		void YtreeItems_Selection_Changed (object sender, EventArgs e)
 		{
