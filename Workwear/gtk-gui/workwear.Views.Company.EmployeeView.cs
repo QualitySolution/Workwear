21896359109d7d6e68c28948986262430f244c0d
Gankov <gav@qsolution.ru>
2020-08-17 11:27:14 +0300

    Файл видимо удалился при мерже.

---
+++ Workwear/gtk-gui/workwear.Views.Company.EmployeeView.cs
@@ -0,0 +1,170 @@
+
+namespace workwear.Views.Company
+{
+	public partial class EmployeeView
+	{
+		private Gtk.VBox dialog1_VBox;
+
+		private Gtk.HBox hbox4;
+
+		private Gtk.Button buttonSave;
+
+		private Gtk.Button buttonCancel;
+
+		private Gtk.VSeparator vseparator1;
+
+		private QSOrmProject.EnumMenuButton enumPrint;
+
+		private Gtk.HBox hbox1;
+
+		private Gtk.Table table1;
+
+		private Gamma.GtkWidgets.yEntry yentryPersonnelNumber;
+
+		private Gamma.Widgets.yEnumComboBox ycomboWinterShoesStd;
+
+		private Gamma.GtkWidgets.yComboBox ycomboWinterShoesSize;
+
+		private Gamma.Widgets.yEnumComboBox ycomboWearStd;
+
+		private Gamma.GtkWidgets.yComboBox ycomboWearSize;
+
+		private Gamma.GtkWidgets.yComboBox ycomboWearGrowth;
+
+		private Gamma.Widgets.yEnumComboBox ycomboShoesStd;
+
+		private Gamma.GtkWidgets.yComboBox ycomboShoesSize;
+
+		private Gamma.GtkWidgets.yComboBox ycomboMittensSize;
+
+		private Gamma.Widgets.yEnumComboBox ycomboHeaddressStd;
+
+		private Gamma.GtkWidgets.yComboBox ycomboHeaddressSize;
+
+		private Gamma.Widgets.yEnumComboBox ycomboGlovesStd;
+
+		private Gamma.GtkWidgets.yComboBox ycomboGlovesSize;
+
+		private Gamma.GtkWidgets.yLabel labelUser;
+
+		private Gtk.Label label9;
+
+		private Gtk.Label label8;
+
+		private Gtk.Label label7;
+
+		private Gtk.Label label6;
+
+		private Gtk.Label label5;
+
+		private Gtk.Label label4;
+
+		private Gtk.Label label3;
+
+		private Gtk.Label label27;
+
+		private Gtk.Label label26;
+
+		private Gtk.Label label25;
+
+		private Gtk.Label label24;
+
+		private Gtk.Label label23;
+
+		private Gtk.Label label20;
+
+		private Gtk.Label label2;
+
+		private Gtk.Label label19;
+
+		private Gtk.Label label18;
+
+		private Gtk.Label label17;
+
+		private Gtk.Label label16;
+
+		private Gtk.Label label14;
+
+		private Gtk.Label label12;
+
+		private Gtk.Label label11;
+
+		private Gtk.Label label10;
+
+		private Gtk.Label label1;
+
+		private Gtk.HBox hbox7;
+
+		private Gamma.GtkWidgets.yEntry entryId;
+
+		private Gamma.GtkWidgets.yCheckButton checkAuto;
+
+		private Gtk.ScrolledWindow GtkScrolledWindowComments;
+
+		private Gamma.GtkWidgets.yTextView ytextComment;
+
+		private Gamma.GtkWidgets.yEntry entryPatronymic;
+
+		private Gamma.GtkWidgets.yEntry entryLastName;
+
+		private Gamma.GtkWidgets.yEntry entryFirstName;
+
+		private QS.Views.Control.EntityEntry entitySubdivision;
+
+		private QS.Views.Control.EntityEntry entityPost;
+
+		private QS.Views.Control.EntityEntry entityLeader;
+
+		private QS.Views.Control.EntityEntry entityDepartment;
+
+		private QS.Widgets.GtkUI.DatePicker dateHire;
+
+		private QS.Widgets.GtkUI.DatePicker dateDismiss;
+
+		private QS.Widgets.GtkUI.DatePicker dateChangePosition;
+
+		private Gamma.Widgets.yEnumComboBox comboSex;
+
+		private Gtk.VBox vbox2;
+
+		private Gtk.Frame frame1;
+
+		private Gamma.Widgets.yImageViewer yimagePhoto;
+
+		private Gtk.HBox hbox11;
+
+		private Gtk.Button buttonLoadPhoto;
+
+		private Gtk.Button buttonSavePhoto;
+
+		private Gtk.Notebook notebook1;
+
+		private workwear.Views.Company.EmployeeChilds.EmployeeNormsView employeenormsview1;
+
+		private Gtk.Label label13;
+
+		private workwear.Views.Company.EmployeeChilds.EmployeeWearItemsView employeewearitemsview1;
+
+		private Gtk.Label label15;
+
+		private Gtk.VBox vbox4;
+
+		private workwear.Views.Company.EmployeeChilds.EmployeeListedItemsView employeecardlisteditemsview;
+
+		private Gtk.Label label21;
+
+		private Gtk.VBox vbox3;
+
+		private workwear.Views.Company.EmployeeChilds.EmployeeMovementsView employeemovementsview1;
+
+		private Gtk.Label label22;
+
+		private workwear.Views.Company.EmployeeChilds.EmployeeVacationsView employeevacationsview1;
+
+		private Gtk.Label label28;
+
+		private void Build()
+		{
+		}
+	}
+}
