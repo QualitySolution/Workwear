863bb24edcfbd2bfaa407f59fd5f5161e3687a5e
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-08-21 11:07:52 +0300

    Убраны дубли в методах

--- Workwear/Domain/Stock/Income.cs
+++ Workwear/Domain/Stock/Income.cs
@@ -209,7 +209,7 @@ namespace workwear.Domain.Stock
 			Items.ToList().ForEach(x => x.UpdateOperations(uow, askUser));
 		}
 
-		public virtual void UpdateEmployeeNextIssue()
+		public virtual void UpdateEmployeeWearItems()
 		{
 			EmployeeCard.UpdateNextIssue(Items.Select(x => x.Nomenclature.Type).ToArray());
 			EmployeeCard.FillWearRecivedInfo(UoW);
