834419a0302cc5ff7a139ae2ae731dfe3f1d36fa
Gankov <gav@qsolution.ru>
2021-02-08 20:26:57 +0300

    Удалил лишний мусорный код, он вообще не нужен.

--- Workwear/ViewModels/Stock/WarehouseViewModel.cs
+++ Workwear/ViewModels/Stock/WarehouseViewModel.cs
@@ -1,33 +1,16 @@
-﻿using System;
-using Autofac;
-using QS.Dialog;
-using QS.DomainModel.UoW;
+﻿using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
-using QS.Tdi;
-using QS.ViewModels.Control.EEVM;
 using QS.ViewModels.Dialog;
-using workwear.Domain.Company;
 using workwear.Domain.Stock;
 
 namespace workwear.ViewModels.Stock
 {
-	public class WarehouseViewModel : LegacyEntityDialogViewModelBase<Warehouse>
+	public class WarehouseViewModel : EntityDialogViewModelBase<Warehouse>
 	{
-		public EntityEntryViewModel<Subdivision> SubdivisionEntryViewModel;
-		public ILifetimeScope AutofacScope;
-		public ITdiCompatibilityNavigation navigationManager;
-		public ITdiCompatibilityNavigation tdiNavigationManager;
-		private readonly CommonMessages commonMessages;
-
-		public WarehouseViewModel(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, ITdiTab myTab, ITdiCompatibilityNavigation navigationManager, ILifetimeScope autofacScope, CommonMessages commonMessages) : base(uowBuilder, unitOfWorkFactory, myTab, navigationManager)
+		public WarehouseViewModel(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, INavigationManager navigationManager) : base(uowBuilder, unitOfWorkFactory, navigationManager)
 		{
-			this.tdiNavigationManager = navigationManager ?? throw new ArgumentNullException(nameof(navigationManager));
-			this.AutofacScope = autofacScope ?? throw new ArgumentNullException(nameof(autofacScope));
-			this.commonMessages = commonMessages;
-			var entryBuilder = new LegacyEEVMBuilderFactory<Warehouse>(this, TdiTab, Entity, UoW, navigationManager) {
-				AutofacScope = AutofacScope
-			};
+
 		}
 	}
 }
