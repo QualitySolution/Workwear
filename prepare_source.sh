a37f474f2c3156cc3f09da9c45833b92ef80821d
Gankov <gav@qsolution.ru>
2021-06-28 19:58:01 +0300

    Перенес изменения из 2.6 чтобы удобнее мержить.

--- prepare_source.sh
+++ prepare_source.sh
@@ -3,15 +3,23 @@ echo "Что делаем?"
 echo "1) git pull"
 echo "2) nuget restore"
 echo "3) cleanup packages directories"
-echo "4) cleanup bin directories"
+echo "4) cleanup bin and obj directories"
+echo "5) rebuild dependent dotnet libraries"
 echo "Можно вызывать вместе, например git+nuget=12"
 read case;
+
+cd "$(dirname "$0")"
 
 case $case in
     *4*)
 rm -v -f -R ./Workwear/bin/*
 rm -v -f -R ../QSProjects/*/bin/*
 rm -v -f -R ../My-FyiReporting/*/bin/*
+rm -v -f -R ../GammaBinding/*/bin/*
+rm -v -f -R ./Workwear/obj/*
+rm -v -f -R ../QSProjects/*/obj/*
+rm -v -f -R ../My-FyiReporting/*/obj/*
+rm -v -f -R ../GammaBinding/*/obj/*
 ;;&
     *3*)
 rm -v -f -R ./packages/*
@@ -34,6 +42,10 @@ cd ../Workwear
 nuget restore Workwear.sln;
 nuget restore ../QSProjects/QSProjectsLib.sln;
 nuget restore ../My-FyiReporting/MajorsilenceReporting-Linux-GtkViewer.sln
+;;&
+    *5*)
+dotnet publish /p:Platform=x86 ../QSProjects/QS.Cloud.Client/QS.Cloud.Client.csproj
+dotnet publish /p:Platform=x86 ./QS.Cloud.WearLk.Client/QS.Cloud.WearLk.Client.csproj  
 ;;&
 esac
 
