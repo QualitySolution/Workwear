e961def9d540baade8f0e7f47ea56feebca9b486
Gankov <gav@qsolution.ru>
2021-09-09 01:15:58 +0300

    Удалил не актульный скрипт.

--- Workwear/Updates/2.6.sql
+++
@@ -1,2 +0,0 @@
-﻿-- Удаляем больше не используемый по умолчанию параметр базы edition
-DELETE FROM `base_parameters` WHERE `base_parameters`.`name` = 'edition' AND `base_parameters`.`str_value` = 'gpl'
