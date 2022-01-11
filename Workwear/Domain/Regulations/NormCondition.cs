97fc00b28d77c3cf7c4417931c9629b9105682ac
Ильяс <mi@qsulution.ru>
2022-01-11 09:57:36 +0300

    Добавлена доменная модель и модель для базы данных

---
+++ Workwear/Domain/Regulations/NormCondition.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QS.DomainModel.Entity;
+
+namespace workwear.Domain.Regulations
+{
+	public class NormCondition : PropertyChangedBase, IDomainObject
+	{
+		public virtual int Id { get; set; }
+
+		private string name;
+		[Display(Name = "Название")]
+		public virtual string Name {
+			get => name;
+			set => SetField(ref name, value);
+		}
+
+		public SexNormCondition SexNormCondition;
+	}
+	public enum SexNormCondition
+	{
+		[Display(Name = "Для всех")]
+		ForAll,
+		[Display(Name = "Только мужчинам")]
+		OnlyMen,
+		[Display(Name = "Только жешщинам")]
+		OnlyWomen
+	}
+}
