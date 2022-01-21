69c7dd15846860b03fdf2949885ce20be14dbf66
Gankov <gav@qsolution.ru>
2022-01-21 14:23:28 +0300

    Revert "Исправляем сбоку 2.4"
    This reverts commit 2d1b3b852e22cbf671795b5ad9cc1ed7f8327d35.

--- WorkwearTest/Operations/EmployeeIssueOperationTest.cs
+++ WorkwearTest/Operations/EmployeeIssueOperationTest.cs
@@ -108,26 +108,25 @@ namespace WorkwearTest.Operations
 
 		[Test(Description = "Дата начала использования не должна сдвигаться если мы не просим ее сдвинуть.(реальный случай, пользователи не понимаю почему срок носки сдвинут относительно выдачи, если вдруг будут запросы на старое поведение, возможно нужно сделать настроку для этого, но по умолчанию оставить проверяемое этим тестом.)")]
 		[Category("real case")]
-		[Ignore("ProtectionTools нет в 2.4")]
 		public void RecalculateDatesOfIssueOperation_DontMoveStartOfUseTest()
 		{
 			var employee = Substitute.For<EmployeeCard>();
 
-			//var protectionTools = Substitute.For<ProtectionTools>();
+			var protectionTools = Substitute.For<ProtectionTools>();
 
 			var norm = new NormItem()
 			{
 				Amount = 1,
 				NormPeriod = NormPeriodType.Month,
 				PeriodCount = 24,
-				// ProtectionTools = protectionTools
+				ProtectionTools = protectionTools
 			};
 
 			var nomenclature = Substitute.For<Nomenclature>();
 			nomenclature.TypeName.Returns("fake");
 
 			var operation1 = Substitute.For<EmployeeIssueOperation>();
-			// operation1.ProtectionTools.Returns(protectionTools);
+			operation1.ProtectionTools.Returns(protectionTools);
 			operation1.OperationTime.Returns(new DateTime(2019, 4, 30));
 			operation1.StartOfUse.Returns(new DateTime(2019, 4, 30));
 			operation1.AutoWriteoffDate.Returns(new DateTime(2021, 4, 30));
@@ -136,7 +135,7 @@ namespace WorkwearTest.Operations
 
 			var issue = new EmployeeIssueOperation
 			{
-				// ProtectionTools = protectionTools,
+				ProtectionTools = protectionTools,
 				Employee = employee,
 				Nomenclature = nomenclature,
 				NormItem = norm,
