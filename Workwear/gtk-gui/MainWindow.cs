64ed50579b534983b66cce60da4a0240882f761a
Ильяс <mi@qsulution.ru>
2022-02-03 09:24:42 +0300

    Удалить старую ведомость в виде отчета

--- Workwear/gtk-gui/MainWindow.cs
+++ Workwear/gtk-gui/MainWindow.cs
@@ -672,7 +672,6 @@ public partial class MainWindow
 		this.ActionUpdate.Activated += new global::System.EventHandler(this.OnActionUpdateActivated);
 		this.ActionSN.Activated += new global::System.EventHandler(this.OnActionSNActivated);
 		this.ActionNorms.Activated += new global::System.EventHandler(this.OnActionNormsActivated);
-		this.Action13.Activated += new global::System.EventHandler(this.OnAction13Activated);
 		this.ActionRequestSheet.Activated += new global::System.EventHandler(this.OnActionRequestSheetActivated);
 		this.ActionAverageAnnualNeed.Activated += new global::System.EventHandler(this.OnActionYearRequestSheetActivated);
 		this.ActionStockDocs.Activated += new global::System.EventHandler(this.OnActionStockDocsActivated);
