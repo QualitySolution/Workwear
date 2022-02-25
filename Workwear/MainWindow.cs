92da69c617149dd5fb9d28dda5737bc9cb5d798d
Ильяс <mi@qsulution.ru>
2022-02-25 09:45:17 +0300

    Добавил сокрытие истории для бесплатных версий

--- Workwear/MainWindow.cs
+++ Workwear/MainWindow.cs
@@ -189,6 +189,7 @@ public partial class MainWindow : Gtk.Window
 		ActionBatchProcessing.Visible = FeaturesService.Available(WorkwearFeature.BatchProcessing);
 		ActionConversatoins.Visible = FeaturesService.Available(WorkwearFeature.Communications);
 		ActionNotificationTemplates.Visible = FeaturesService.Available(WorkwearFeature.Communications);
+		ActionHistory.Visible = FeaturesService.Available(WorkwearFeature.HistoryLog);
 	}
 	#endregion
 
