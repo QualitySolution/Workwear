c87c6f10698d8bc85426fb2fdd6b407fd950b25c
Ильяс <mi@qsulution.ru>
2022-03-15 16:40:01 +0300

    Для тестов

--- Workwear.Sql/Scripts/2.6.sql
+++ Workwear.Sql/Scripts/2.6.sql
@@ -203,7 +203,6 @@ ADD CONSTRAINT `fk_issuance_sheet_items_7`
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
-﻿#Удаляем пустые строки из размеров и роста в операциях. Это приводит к неоднозначности в запросах
 UPDATE `operation_warehouse` SET `size`= NULL WHERE `size` = "";
 UPDATE `operation_warehouse` SET `growth`= NULL WHERE `growth` = "";
 UPDATE `operation_issued_by_employee` SET `size`= NULL WHERE `size` = "";
@@ -212,7 +211,7 @@ UPDATE `operation_issued_by_employee` SET `growth`= NULL WHERE `growth` = "";
 #У таблицы operation_issued_by_employee поменялась немного идеология теперь protection_tools_id является главным значением для выборки вместо номеклатуры
 #Поэтому копируем для возвратов и списаний protection_tools_id из операций выдачи
 UPDATE `operation_issued_by_employee` AS operation
-    LEFT JOIN operation_issued_by_employee AS issued ON issued.id = operation.issued_operation_id
+    LEFT JOIN `operation_issued_by_employee` AS issued ON issued.id = operation.issued_operation_id
     SET operation.protection_tools_id = issued.protection_tools_id
 WHERE issued.id IS NOT NULL AND operation.protection_tools_id IS NULL;
 
