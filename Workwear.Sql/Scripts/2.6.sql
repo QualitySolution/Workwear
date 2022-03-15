e22fea0b41ada9b7e1472f6dfd2bd016e8765bca
Ильяс <mi@qsulution.ru>
2022-03-15 15:46:38 +0300

    Исправляю падение тестов

--- Workwear.Sql/Scripts/2.6.sql
+++ Workwear.Sql/Scripts/2.6.sql
@@ -203,14 +203,14 @@ ADD CONSTRAINT `fk_issuance_sheet_items_7`
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
-﻿-- Удаляем пустые строки из размеров и роста в операциях. Это приводит к неоднозначности в запросах
+﻿#Удаляем пустые строки из размеров и роста в операциях. Это приводит к неоднозначности в запросах
 UPDATE `operation_warehouse` SET `size`= NULL WHERE size = "";
 UPDATE `operation_warehouse` SET `growth`= NULL WHERE growth = "";
 UPDATE `operation_issued_by_employee` SET `size`= NULL WHERE size = "";
 UPDATE `operation_issued_by_employee` SET `growth`= NULL WHERE growth = "";
 
--- У таблицы operation_issued_by_employee поменялась немного идеология теперь protection_tools_id является главным значением для выборки вместо номеклатуры
--- Поэтому копируем для возвратов и списаний protection_tools_id из операций выдачи
+#У таблицы operation_issued_by_employee поменялась немного идеология теперь protection_tools_id является главным значением для выборки вместо номеклатуры
+#Поэтому копируем для возвратов и списаний protection_tools_id из операций выдачи
 UPDATE `operation_issued_by_employee` operation
     LEFT JOIN operation_issued_by_employee issued ON issued.id = operation.issued_operation_id
     SET operation.protection_tools_id = issued.protection_tools_id
