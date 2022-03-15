70878f81811797674b46b3c6d365eaf2c4eccae8
Ильяс <mi@qsulution.ru>
2022-03-15 16:21:23 +0300

    Для тестов

--- Workwear.Sql/Scripts/2.6.sql
+++ Workwear.Sql/Scripts/2.6.sql
@@ -204,15 +204,15 @@ ADD CONSTRAINT `fk_issuance_sheet_items_7`
   ON UPDATE CASCADE;
 
 ﻿#Удаляем пустые строки из размеров и роста в операциях. Это приводит к неоднозначности в запросах
-UPDATE `operation_warehouse` SET `size`= NULL WHERE size = "";
-UPDATE `operation_warehouse` SET `growth`= NULL WHERE growth = "";
-UPDATE `operation_issued_by_employee` SET `size`= NULL WHERE size = "";
-UPDATE `operation_issued_by_employee` SET `growth`= NULL WHERE growth = "";
+UPDATE `operation_warehouse` SET `size`= NULL WHERE `size` = "";
+UPDATE `operation_warehouse` SET `growth`= NULL WHERE `growth` = "";
+UPDATE `operation_issued_by_employee` SET `size`= NULL WHERE `size` = "";
+UPDATE `operation_issued_by_employee` SET `growth`= NULL WHERE `growth` = "";
 
 #У таблицы operation_issued_by_employee поменялась немного идеология теперь protection_tools_id является главным значением для выборки вместо номеклатуры
 #Поэтому копируем для возвратов и списаний protection_tools_id из операций выдачи
-UPDATE `operation_issued_by_employee` operation
-    LEFT JOIN operation_issued_by_employee issued ON issued.id = operation.issued_operation_id
+UPDATE `operation_issued_by_employee` AS operation
+    LEFT JOIN operation_issued_by_employee AS issued ON issued.id = operation.issued_operation_id
     SET operation.protection_tools_id = issued.protection_tools_id
 WHERE issued.id IS NOT NULL AND operation.protection_tools_id IS NULL;
 
