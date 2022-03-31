f2fc4f8b938f2b3f8c5c32f910dfcdc7165717fe
Ильяс <mi@qsulution.ru>
2022-03-31 14:08:53 +0300

    Убрал стандарт размера из вью номенклатуры

--- Workwear/Views/Stock/NomenclatureView.cs
+++ Workwear/Views/Stock/NomenclatureView.cs
@@ -34,16 +34,7 @@ namespace workwear.Views.Stock
 				.AddBinding(vm => vm.VisibleClothesSex, w => w.Visible)
 				.AddBinding(vm => vm.ClothesSexLabel, w => w.LabelProp)
 				.InitializeFromSource();
-
-			var stdConverter = new SizeStandardCodeConverter ();
-
-			ycomboWearStd.Binding.AddSource(ViewModel)
-				.AddBinding(v => v.SizeStdEnum, w => w.ItemsEnum)
-				.AddBinding(v => v.VisibleSizeStd, w => w.Visible)
-				.InitializeFromSource();
-			ycomboWearStd.Binding.AddBinding (Entity, e => e.SizeStd, w => w.SelectedItemOrNull, stdConverter ).InitializeFromSource ();
-
-			labelSize.Binding.AddBinding(ViewModel, v => v.VisibleSizeStd, w => w.Visible).InitializeFromSource();
+				
 
 			ytextComment.Binding.AddBinding(Entity, e => e.Comment, w => w.Buffer.Text).InitializeFromSource();
 
