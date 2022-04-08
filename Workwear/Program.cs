d56da835e34b79862d07524638a9cc808f5dc50a
Иван <space@qsolution.ru>
2022-04-08 17:59:39 +0300

    Добавил актуализацию версии.

--- Workwear/Program.cs
+++ Workwear/Program.cs
@@ -78,7 +78,7 @@ namespace workwear
 			"\n" +
 			"Для установки собственного сервера обратитесь к документации.";
 			Login.CreateDBHelpTooltip = "Инструкция по установке сервера MySQL";
-			Login.CreateDBHelpUrl = "http://doc.qsolution.ru/workwear/2.6/install.html#InstallDBServer";
+			Login.CreateDBHelpUrl = "http://doc.qsolution.ru/workwear/" + UnhandledExceptionHandler.ApplicationInfo.Version.ToString(2) +"/install.html#InstallDBServer";
 			LoginDialog.GetDBCreator = scopeLoginTime.Resolve<IDBCreator>;
 
 			LoginDialog.UpdateFromGConf ();
