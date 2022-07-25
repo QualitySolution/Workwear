1e6f16c9c8a32f764432ca2a70998883dc26877e
Ilyas <neugolok@outlook.com>
2022-07-25 16:08:17 +0500

    Добавил виджет для обработки сообщений

---
+++ Workwear/ViewModels/Communications/ClaimsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Validation;
+using QS.ViewModels.Dialog;
+
+namespace workwear.ViewModels.Communications 
+{
+	public class ClaimsViewModel : UowDialogViewModelBase {
+		public ClaimsViewModel(
+			IUnitOfWorkFactory unitOfWorkFactory, 
+			INavigationManager navigation, 
+			IValidator validator = null, 
+			string UoWTitle = "Обращения сотрудников") : base(unitOfWorkFactory, navigation, validator, UoWTitle) 
+		{
+		}
+	}
+}
---
+++ Workwear/Views/Communications/ClaimsView.cs
@@ -0,0 +1,13 @@
+﻿using QS.Views.Dialog;
+using workwear.ViewModels.Communications;
+
+namespace workwear.Views.Communications 
+{
+	public partial class ClaimsView : DialogViewBase<ClaimsViewModel> 
+	{
+		public ClaimsView(ClaimsViewModel viewModel) : base(viewModel) 
+		{
+			this.Build();
+		}
+	}
+}
