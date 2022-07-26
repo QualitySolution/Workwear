5ed4717261eb86b1e463f985e6ce589bed1a7fa4
Ilyas <neugolok@outlook.com>
2022-07-26 10:31:23 +0500

    Делаю привязку данных во вьюмодели

--- Workwear/ViewModels/Communications/ClaimsViewModel.cs
+++ Workwear/ViewModels/Communications/ClaimsViewModel.cs
@@ -1,17 +1,61 @@
-﻿using QS.DomainModel.UoW;
+﻿using System;
+using System.Collections.Generic;
+using QS.Cloud.WearLk.Client;
+using QS.Cloud.WearLk.Manage;
+using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Validation;
 using QS.ViewModels.Dialog;
+using workwear.Repository.Company;
 
 namespace workwear.ViewModels.Communications 
 {
-	public class ClaimsViewModel : UowDialogViewModelBase {
+	public class ClaimsViewModel : UowDialogViewModelBase 
+	{
+		
+		private readonly ClaimsManager claimsManager;
+		private readonly EmployeeRepository employeeRepository;
+		
 		public ClaimsViewModel(
 			IUnitOfWorkFactory unitOfWorkFactory, 
-			INavigationManager navigation, 
+			INavigationManager navigation,
+			ClaimsManager claimsManager,
+			EmployeeRepository employeeRepository,
 			IValidator validator = null, 
-			string UoWTitle = "Обращения сотрудников") : base(unitOfWorkFactory, navigation, validator, UoWTitle) {
+			string UoWTitle = "Обращения сотрудников"
+			) : base(unitOfWorkFactory, navigation, validator, UoWTitle) 
+		{
 			Title = "Обращения сотрудников";
+			this.claimsManager = claimsManager;
+			this.employeeRepository = employeeRepository;
+		}
+		
+
+		#region View
+
+		private IList<Claim> Claims { get; set; }
+		public Claim SelectClaim { get; set; }
+		public ClaimMessage MessagesSelectClaims { get; set; }
+		public ClaimState SelectClaimState { get; set; }
+		public bool ShowClosed { get; set; } 
+		public string TextMessage { get; set; }
+
+		#endregion
+
+		#region ViewMethods
+
+		public object GetClaims() {
+			throw new System.NotImplementedException();
+		}
+
+		#endregion
+
+		public void Send(object sender, EventArgs eventArgs) {
+			throw new NotImplementedException();
+		}
+
+		public void ChangeStatusClaim(object sender, EventArgs e) {
+			throw new NotImplementedException();
 		}
 	}
 }
--- Workwear/Views/Communications/ClaimsView.cs
+++ Workwear/Views/Communications/ClaimsView.cs
@@ -1,4 +1,7 @@
-﻿using QS.Views.Dialog;
+﻿using System;
+using Gamma.GtkWidgets;
+using QS.Cloud.WearLk.Manage;
+using QS.Views.Dialog;
 using workwear.ViewModels.Communications;
 
 namespace workwear.Views.Communications 
@@ -8,6 +11,43 @@ namespace workwear.Views.Communications
 		public ClaimsView(ClaimsViewModel viewModel) : base(viewModel) 
 		{
 			this.Build();
+
+			yenumcombobox2.ItemsEnum = typeof(ClaimState);
+			yenumcombobox2.Binding.AddBinding(
+				ViewModel, 
+				vm => vm.SelectClaimState,
+				v => v.SelectedItem);
+			ytreeClaims.ColumnsConfig = ColumnsConfigFactory.Create<Claim>()
+				.AddColumn("Обращение").AddTextRenderer(c => c.Title)
+				.RowCells()
+				.AddSetter<Gtk.CellRendererText>((c, x) => c.Foreground = GetRowColor(x))
+				.Finish();
+			ytreeClaims.ItemsDataSource = ViewModel.GetClaims();
+			ytreeClaimMessages.ColumnsConfig = ColumnsConfigFactory.Create<ClaimMessage>()
+				.AddColumn("Автор").AddTextRenderer(c => c.SenderName)
+				.AddColumn("Текст").AddTextRenderer(c => c.Text)
+				.AddColumn("Время").AddTextRenderer(c => c.SendTime.ToString())
+				.RowCells()
+				.AddSetter<Gtk.CellRendererText>((c, x) => c.Foreground = x.UserRead ? String.Empty : "gray")
+				.Finish();
+			yentryMessage.Binding
+				.AddBinding(ViewModel, vm => vm.TextMessage, w => w.Text)
+				.InitializeFromSource();
+			ybuttonSend.Clicked += ViewModel.Send;
+			ybuttonChangeStatus.Clicked += ViewModel.ChangeStatusClaim;
+		}
+
+		private string GetRowColor(Claim claim) {
+			switch(claim.ClaimState) {
+				case ClaimState.Closed:
+					return "gray";
+				case ClaimState.WaitSupport:
+					return "red";
+				case  ClaimState.WaitUser:
+					return "yellow";
+				default:
+					throw new ArgumentException();
+			}
 		}
 	}
 }
