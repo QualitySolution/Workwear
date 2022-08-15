dab4b50ccfaafcce506011a760ca122b635a4135
Ilyas <neugolok@outlook.com>
2022-08-15 17:07:51 +0500

    Добавил парсер

---
+++ Workwear/Tools/Import/ReaderDocumentFromXml1C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace workwear.Tools.Import 
+{
+	public class ReaderDocumentFromXml1C 
+	{
+		private readonly string fileName;
+		
+		public ReaderDocumentFromXml1C(string fileName) 
+		{
+			this.fileName = fileName;
+		}
+
+		public DateTime GetDateInFile() 
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
