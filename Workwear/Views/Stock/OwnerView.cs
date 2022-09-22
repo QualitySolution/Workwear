f61e15747afd9ed981d7a7804d60af9243117034
Ilyas <neugolok@outlook.com>
2022-09-22 15:45:10 +0500

    Добавил вью для владельцев

---
+++ Workwear/Views/Stock/OwnerView.cs
@@ -0,0 +1,22 @@
+﻿using QS.Views.Dialog;
+using Workwear.Domain.Stock;
+using Workwear.ViewModels.Stock;
+
+namespace Workwear.Views.Stock 
+{
+	public partial class OwnerView : EntityDialogViewBase<OwnerViewModel, Owner>
+	{
+		public OwnerView(OwnerViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+			
+			yentryName.Binding
+				.AddBinding(Entity, e => e.Name, w => w.Text)
+				.InitializeFromSource();
+			
+			ytextviewComment.Binding
+				.AddBinding(Entity, e => e.Description, w => w.Buffer)
+				.InitializeFromSource();
+		}
+	}
+}
