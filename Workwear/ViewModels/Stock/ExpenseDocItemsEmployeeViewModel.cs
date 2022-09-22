e13f93cb39aa53cbe5b710139f7a8d14dfeebde9
Gankov <gav@qsolution.ru>
2022-09-22 14:46:43 +0300

    Исправлена ошбика возникающая при удалении строки выдачи
    В случае если в ту же сессию была добавлена новая строка. падало из за несохраненного документа целиком.

--- Workwear/ViewModels/Stock/ExpenseDocItemsEmployeeViewModel.cs
+++ Workwear/ViewModels/Stock/ExpenseDocItemsEmployeeViewModel.cs
@@ -139,8 +139,11 @@ namespace workwear.ViewModels.Stock
 
 		public void Delete(ExpenseItem item)
 		{
-			if(item.Id > 0)
+			if(item.Id > 0) {
+				if(Entity.Items.Any(x => x.Id == 0))
+					expenseEmployeeViewModel.Save(); //Сохраняем документ если есть добавленные строки. Иначе получим исключение.
 				deleteService.DeleteEntity<ExpenseItem>(item.Id, UoW, () => Entity.RemoveItem(item));
+			}
 			else
 				Entity.RemoveItem(item);
 			CalculateTotal();
