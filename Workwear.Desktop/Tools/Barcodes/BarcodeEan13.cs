ebb3bbe4569f217e2c9e0805f584b625e02074c6
Ilyas <neugolok@outlook.com>
2022-09-30 13:02:55 +0500

    create barcode

---
+++ Workwear.Desktop/Tools/Barcodes/BarcodeEan13.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Workwear.Tools.Barcodes 
+{
+	public class BarcodeEan13 
+	{
+		public BarcodeEan13(string value) {
+			if(value.Length > 13)
+				throw new ArgumentOutOfRangeException();
+			Value = value;
+		}
+		public string Value { get; }
+	}
+}
---
+++ Workwear.Desktop/Tools/Barcodes/BarcodeService.cs
@@ -0,0 +1,17 @@
+﻿namespace Workwear.Tools.Barcodes 
+{
+	public class BarcodeService 
+	{
+		public string ClientCode { get; } //2
+		public string BaseCode { get; } //3
+		
+		public BarcodeEan13 Create() 
+		{
+			return new BarcodeEan13($"{ClientCode}{BaseCode}{GetFreeProductCode()}");
+		}
+
+		public string GetFreeProductCode() {
+			return "00000001";
+		}
+	}
+}
