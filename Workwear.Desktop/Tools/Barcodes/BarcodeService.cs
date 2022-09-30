5a70e30b440f74481a0c1dc05bcb880e3fa9c0c6
Ilyas <neugolok@outlook.com>
2022-09-30 16:40:29 +0500

    Исправление

--- Workwear.Desktop/Tools/Barcodes/BarcodeService.cs
+++ Workwear.Desktop/Tools/Barcodes/BarcodeService.cs
@@ -1,17 +1,29 @@
-﻿namespace Workwear.Tools.Barcodes 
+﻿using System;
+using System.Linq;
+
+namespace Workwear.Tools.Barcodes 
 {
 	public class BarcodeService 
 	{
-		public string ClientCode { get; } //2
-		public string BaseCode { get; } //3
-		
-		public BarcodeEan13 Create() 
-		{
-			return new BarcodeEan13($"{ClientCode}{BaseCode}{GetFreeProductCode()}");
+		public int BaseCode { get; } //2000-2999
+
+		public BarcodeEan13 Create() {
+			var freeCode = GetFreeProductCode();
+			return new BarcodeEan13($"{BaseCode}{freeCode:D8}{CheckDigit(BaseCode, freeCode)}");
 		}
 
-		public string GetFreeProductCode() {
-			return "00000001";
+		private int GetFreeProductCode() {
+			return 1;
 		}
+
+		private int CheckDigit(int baseCode, int code) {
+			var result = SumDigit(Int32.Parse(baseCode.ToString() + code));
+			while(result > 9) {
+				result = SumDigit(result);
+			}
+			return result;
+		}
+
+		private int SumDigit(int digit) => digit.ToString().Split().Sum(Int32.Parse);
 	}
 }
