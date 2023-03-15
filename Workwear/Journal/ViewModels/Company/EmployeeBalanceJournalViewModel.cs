a3cad10d958e80528db5ef50b9c48a3e3b28ba7b
Gankov <gav@qsolution.ru>
2023-03-15 18:59:28 +0300

    Показываем в списках для списания, даже элементы если чего то не хватает, не существенного.
    Например нет единицы измерения или типа номеклатуры.
    Словил из за этого баг в тестах.

--- Workwear/Journal/ViewModels/Company/EmployeeBalanceJournalViewModel.cs
+++ Workwear/Journal/ViewModels/Company/EmployeeBalanceJournalViewModel.cs
@@ -75,11 +75,11 @@ namespace workwear.Journal.ViewModels.Company
 			);
 			if (Filter.Employee != null)
 				query
-					.JoinAlias(() => expenseOperationAlias.Nomenclature, () => nomenclatureAlias)
+					.JoinAlias(() => expenseOperationAlias.Nomenclature, () => nomenclatureAlias, JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.WearSize, () => sizeAlias, JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.Height, () => heightAlias, JoinType.LeftOuterJoin)
-					.JoinAlias(() => nomenclatureAlias.Type, () => itemTypesAlias)
-					.JoinAlias(() => itemTypesAlias.Units, () => unitsAlias)
+					.JoinAlias(() => nomenclatureAlias.Type, () => itemTypesAlias, JoinType.LeftOuterJoin)
+					.JoinAlias(() => itemTypesAlias.Units, () => unitsAlias, JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.WarehouseOperation, () => warehouseOperationAlias,
 						JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.Employee, () => employeeCardAlias)
@@ -102,11 +102,11 @@ namespace workwear.Journal.ViewModels.Company
 						.Select(balance).WithAlias(() => resultAlias.Balance));
 			else {
 				query
-					.JoinAlias(() => expenseOperationAlias.Nomenclature, () => nomenclatureAlias)
+					.JoinAlias(() => expenseOperationAlias.Nomenclature, () => nomenclatureAlias, JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.WearSize, () => sizeAlias, JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.Height, () => heightAlias, JoinType.LeftOuterJoin)
-					.JoinAlias(() => nomenclatureAlias.Type, () => itemTypesAlias)
-					.JoinAlias(() => itemTypesAlias.Units, () => unitsAlias)
+					.JoinAlias(() => nomenclatureAlias.Type, () => itemTypesAlias, JoinType.LeftOuterJoin)
+					.JoinAlias(() => itemTypesAlias.Units, () => unitsAlias, JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.WarehouseOperation, () => warehouseOperationAlias,
 						JoinType.LeftOuterJoin)
 					.JoinAlias(() => expenseOperationAlias.Employee, () => employeeCardAlias)
