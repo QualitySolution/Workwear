8c900d98e4b931061edcdaff637795a9edaaf7bb
Иван <space@qsolution.ru>
2023-03-22 16:48:02 +0300

    Добавил View

--- Workwear/Views/Stock/InspectionView.cs
+++ Workwear/Views/Stock/InspectionView.cs
@@ -1,11 +1,12 @@
-﻿using QS.Views.Dialog;
+﻿using System;
+using QS.Views.Dialog;
 using Workwear.Domain.Stock.Documents;
 using Workwear.ViewModels.Stock;
 
 namespace Workwear.Views.Stock {
-	public class InspectionView  : EntityDialogViewBase<InspectionViewModel, Inspection> {
-		public InspectionView(InspectionViewModel viewModel) : base(viewModel)
-		{
+	public partial class InspectionView : EntityDialogViewBase<InspectionViewModel, Inspection> {
+		public InspectionView(InspectionViewModel viewModel) : base(viewModel) {
+			this.Build();
 		}
 	}
 }
