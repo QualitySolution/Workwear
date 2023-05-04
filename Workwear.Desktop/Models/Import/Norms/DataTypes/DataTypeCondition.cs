0723463ec292401c8a8344fd0ff236aae2d04286
Иван <space@qsolution.ru>
2023-05-04 15:37:14 +0300

    Добавил в импорт учёт условий нормы

---
+++ Workwear.Desktop/Models/Import/Norms/DataTypes/DataTypeCondition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Workwear.Domain.Regulations;
+
+namespace Workwear.Models.Import.Norms.DataTypes{
+	public class DataTypeCondition : DataTypeNormBase {
+
+
+		public DataTypeCondition( IList<NormCondition> conditions) {
+			ColumnNameKeywords.Add("условия");
+			ColumnNameKeywords.Add("условие");
+			ColumnNameKeywords.Add("сезон");
+			this.conditions = conditions;
+			Data = DataTypeNorm.Condition;
+		}
+
+		private IList<NormCondition> conditions;
+
+		public override void CalculateChange(SheetRowNorm row, ExcelValueTarget target) {
+			var value = row.CellStringValue(target) ?? "";
+			row.AddColumnChange(target, GetChangeState(row, value));
+		}
+
+		private ChangeState GetChangeState(SheetRowNorm row, string value) {
+			if(String.IsNullOrWhiteSpace(value)) {
+				return new ChangeState(ChangeType.NotChanged);
+			}
+
+			var con = conditions?.FirstOrDefault(c => c.Name == value);
+			if(con != null) {
+				row.AddSetValueAction(ValueSetOrder, () => row.NormItem.NormCondition = con);
+				return new ChangeState(ChangeType.NewEntity);
+			}
+			else 
+				return new ChangeState(ChangeType.NotFound );
+		}
+	}
+}
