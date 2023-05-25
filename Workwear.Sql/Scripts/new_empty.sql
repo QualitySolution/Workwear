680271392fcdc88db8f2e75697af27d7f4c282d2
Gankov <gav@qsolution.ru>
2023-05-25 14:56:07 +0300

    Исправил номер версии созданной базы.

--- Workwear.Sql/Scripts/new_empty.sql
+++ Workwear.Sql/Scripts/new_empty.sql
@@ -2017,7 +2017,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('product_name', 'workwear');
-INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('version', '2.8');
+INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('version', '2.8.1');
 INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('DefaultAutoWriteoff', 'True');
 
 COMMIT;
