1b7295522c75e56414636082ad3497be60fdc0ec
Gankov <gav@qsolution.ru>
2023-10-02 15:43:54 +0300

    Добавлена установка подразделения из отдела.

--- Workwear/ViewModels/Company/PostViewModel.cs
+++ Workwear/ViewModels/Company/PostViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using Autofac;
+using QS.Dialog;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
@@ -18,18 +20,19 @@ namespace Workwear.ViewModels.Company
 	public class PostViewModel : EntityDialogViewModelBase<Post>
 	{
 		private readonly FeaturesService featuresService;
-		private readonly ILifetimeScope autofacScope;
+		private readonly IInteractiveQuestion interactive;
 
 		public PostViewModel(
 			IEntityUoWBuilder uowBuilder,
 			IUnitOfWorkFactory unitOfWorkFactory,
 			INavigationManager navigation,
 			FeaturesService featuresService,
+			IInteractiveQuestion interactive,
 			ILifetimeScope autofacScope,
 			IValidator validator = null) : base(uowBuilder, unitOfWorkFactory, navigation, validator)
 		{
 			this.featuresService = featuresService ?? throw new ArgumentNullException(nameof(featuresService));
-			this.autofacScope = autofacScope ?? throw new ArgumentNullException(nameof(autofacScope));
+			this.interactive = interactive ?? throw new ArgumentNullException(nameof(interactive));
 			var builder = new CommonEEVMBuilderFactory<Post>(this, Entity, UoW, NavigationManager, autofacScope);
 
 			EntrySubdivision = builder.ForProperty(x => x.Subdivision)
@@ -50,21 +53,29 @@ namespace Workwear.ViewModels.Company
 			EntryCostCenter = builder.ForProperty(x => x.CostCenter)
 									.MakeByType()
 									.Finish();
+			Entity.PropertyChanged += Entity_PropertyChanged;
 		}
-
+		
 		#region Controls
-
 		public EntityEntryViewModel<Subdivision> EntrySubdivision;
 		public EntityEntryViewModel<Department> EntryDepartment;
 		public EntityEntryViewModel<Profession> EntryProfession;
 		public EntityEntryViewModel<CostCenter> EntryCostCenter;
-
 		#endregion
 
 		#region Visible
-
 		public bool VisibleCostCenter => featuresService.Available(WorkwearFeature.CostCenter);
+		#endregion
 
+		#region События
+		private void Entity_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+			if(e.PropertyName == nameof(Entity.Department)
+			   && Entity.Department?.Subdivision != null
+			   && Entity.Department?.Subdivision != Entity.Subdivision
+			   && interactive.Question($"Изменить так же подразделение на {Entity.Department.Subdivision.Name}?")) {
+				Entity.Subdivision = Entity.Department.Subdivision;
+			}
+		}
 		#endregion
 	}
 }
