44d27027d49c17ca30c130f283b4acba469ffe24
Иван <space@qsolution.ru>
2023-10-16 08:19:21 +0300

    Исправление вьюшек

--- Workwear/gtk-gui/Workwear.Views.Company.EmployeeGroupItemsView.cs
+++ Workwear/gtk-gui/Workwear.Views.Company.EmployeeGroupItemsView.cs
@@ -4,7 +4,7 @@ namespace Workwear.Views.Company
 {
 	public partial class EmployeeGroupItemsView
 	{
-		private global::Gtk.VBox vbox2;
+		private global::Gtk.VBox vbox1;
 
 		private global::Gtk.ScrolledWindow GtkScrolledWindow;
 
@@ -23,10 +23,10 @@ namespace Workwear.Views.Company
 			global::Stetic.BinContainer.Attach(this);
 			this.Name = "Workwear.Views.Company.EmployeeGroupItemsView";
 			// Container child Workwear.Views.Company.EmployeeGroupItemsView.Gtk.Container+ContainerChild
-			this.vbox2 = new global::Gtk.VBox();
-			this.vbox2.Name = "vbox2";
-			this.vbox2.Spacing = 6;
-			// Container child vbox2.Gtk.Box+BoxChild
+			this.vbox1 = new global::Gtk.VBox();
+			this.vbox1.Name = "vbox1";
+			this.vbox1.Spacing = 6;
+			// Container child vbox1.Gtk.Box+BoxChild
 			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow();
 			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
 			this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
@@ -35,10 +35,10 @@ namespace Workwear.Views.Company
 			this.ytreeItems.CanFocus = true;
 			this.ytreeItems.Name = "ytreeItems";
 			this.GtkScrolledWindow.Add(this.ytreeItems);
-			this.vbox2.Add(this.GtkScrolledWindow);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox2[this.GtkScrolledWindow]));
+			this.vbox1.Add(this.GtkScrolledWindow);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.GtkScrolledWindow]));
 			w2.Position = 0;
-			// Container child vbox2.Gtk.Box+BoxChild
+			// Container child vbox1.Gtk.Box+BoxChild
 			this.hbox5 = new global::Gtk.HBox();
 			this.hbox5.Name = "hbox5";
 			this.hbox5.Spacing = 6;
@@ -69,12 +69,12 @@ namespace Workwear.Views.Company
 			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.hbox5[this.buttonRemove]));
 			w6.Position = 1;
 			w6.Expand = false;
-			this.vbox2.Add(this.hbox5);
-			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox2[this.hbox5]));
+			this.vbox1.Add(this.hbox5);
+			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbox5]));
 			w7.Position = 1;
 			w7.Expand = false;
 			w7.Fill = false;
-			this.Add(this.vbox2);
+			this.Add(this.vbox1);
 			if ((this.Child != null))
 			{
 				this.Child.ShowAll();
