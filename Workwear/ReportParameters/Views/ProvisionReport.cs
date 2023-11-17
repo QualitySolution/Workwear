576e256e24d6200789e0179949945c53f4dc3174
Иван <space@qsolution.ru>
2023-11-17 12:38:20 +0300

    Вьюшка отчёта

---
+++ Workwear/ReportParameters/Views/ProvisionReport.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Workwear.ReportParameters.Views {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ProvisionReport : Gtk.Bin {
+		public ProvisionReport() {
+			this.Build();
+		}
+	}
+}
