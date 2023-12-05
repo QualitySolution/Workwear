49faa683117bcd2d65dda27f009be34a77e5d1c1
Ярик <y.matveev@qsolution.ru>
2023-12-05 19:26:24 +0300

    Печать штрихкодов в Диалоговом окне штрихкода

--- Workwear/ViewModels/Stock/BarcodeViewModel.cs
+++ Workwear/ViewModels/Stock/BarcodeViewModel.cs
@@ -1,24 +1,45 @@
-﻿using QS.DomainModel.UoW;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentNHibernate.Data;
+using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
+using QS.Report;
+using QS.Report.ViewModels;
 using QS.Validation;
 using QS.ViewModels.Dialog;
+using Workwear.Domain.Operations;
 using Workwear.Domain.Stock;
+using Workwear.Domain.Stock.Documents;
+using Workwear.Tools.Features;
 
 namespace Workwear.ViewModels.Stock 
 {
-	public class BarcodeViewModel : EntityDialogViewModelBase<Barcode>
-	{
+	public class BarcodeViewModel : EntityDialogViewModelBase<Barcode> {
+		
 		public BarcodeViewModel(
 			IEntityUoWBuilder uowBuilder, 
 			IUnitOfWorkFactory unitOfWorkFactory, 
-			INavigationManager navigation, 
+			INavigationManager navigation,
 			IValidator validator = null) : base(uowBuilder, unitOfWorkFactory, navigation, validator)
 		{
-
+			
 		}
+		
+		public void PrintBarcodes() {
+			var reportInfo = new ReportInfo {
+				Title = "Штрихкоды",
+				Identifier = "Barcodes.BarcodeFromEmployeeIssue",
+				Parameters = new Dictionary<string, object> {
+					{"operations", Entity.BarcodeOperations.Select(x => x.EmployeeIssueOperation.Id).ToArray()}
+				}
+			};
 
-
+			NavigationManager.OpenViewModel<RdlViewerViewModel, ReportInfo>(null, reportInfo);
+		}
+		
+		
 		#region ViewProperty
 
 
