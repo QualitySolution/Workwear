69442c34be1781218a73acfd13f23316199e551b
Иван <space@qsolution.ru>
2023-12-06 18:49:51 +0300

    Добавил другие икоки

--- Workwear/gtk-gui/Workwear.ReportParameters.Views.ChoiceSubdivisionView.cs
+++ Workwear/gtk-gui/Workwear.ReportParameters.Views.ChoiceSubdivisionView.cs
@@ -6,17 +6,15 @@ namespace Workwear.ReportParameters.Views
 	{
 		private global::Gtk.VBox vbox1;
 
-		private global::Gtk.HBox hbox1;
+		private global::Gtk.HBox hbox2;
 
 		private global::Gamma.GtkWidgets.yButton ycheckbuttonChooseAll;
 
 		private global::Gamma.GtkWidgets.yButton ycheckbuttonUnChooseAll;
 
-		private global::Gtk.HBox hbox2;
+		private global::Gamma.GtkWidgets.yEntry yentrySearch;
 
 		private global::Gamma.GtkWidgets.yImage yimage1;
-
-		private global::Gamma.GtkWidgets.yEntry yentrySearch;
 
 		private global::Gtk.ScrolledWindow GtkScrolledWindow;
 
@@ -33,47 +31,33 @@ namespace Workwear.ReportParameters.Views
 			this.vbox1.Name = "vbox1";
 			this.vbox1.Spacing = 6;
 			// Container child vbox1.Gtk.Box+BoxChild
-			this.hbox1 = new global::Gtk.HBox();
-			this.hbox1.Name = "hbox1";
-			this.hbox1.Spacing = 6;
-			// Container child hbox1.Gtk.Box+BoxChild
-			this.ycheckbuttonChooseAll = new global::Gamma.GtkWidgets.yButton();
-			this.ycheckbuttonChooseAll.CanFocus = true;
-			this.ycheckbuttonChooseAll.Name = "ycheckbuttonChooseAll";
-			this.ycheckbuttonChooseAll.UseUnderline = true;
-			this.ycheckbuttonChooseAll.Label = global::Mono.Unix.Catalog.GetString("Выделить всё");
-			this.hbox1.Add(this.ycheckbuttonChooseAll);
-			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.ycheckbuttonChooseAll]));
-			w1.Position = 0;
-			w1.Expand = false;
-			w1.Fill = false;
-			// Container child hbox1.Gtk.Box+BoxChild
-			this.ycheckbuttonUnChooseAll = new global::Gamma.GtkWidgets.yButton();
-			this.ycheckbuttonUnChooseAll.CanFocus = true;
-			this.ycheckbuttonUnChooseAll.Name = "ycheckbuttonUnChooseAll";
-			this.ycheckbuttonUnChooseAll.UseUnderline = true;
-			this.ycheckbuttonUnChooseAll.Label = global::Mono.Unix.Catalog.GetString("Снять выделение");
-			this.hbox1.Add(this.ycheckbuttonUnChooseAll);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.ycheckbuttonUnChooseAll]));
-			w2.Position = 1;
-			w2.Expand = false;
-			w2.Fill = false;
-			this.vbox1.Add(this.hbox1);
-			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbox1]));
-			w3.Position = 0;
-			w3.Expand = false;
-			w3.Fill = false;
-			// Container child vbox1.Gtk.Box+BoxChild
 			this.hbox2 = new global::Gtk.HBox();
 			this.hbox2.Name = "hbox2";
 			this.hbox2.Spacing = 6;
 			// Container child hbox2.Gtk.Box+BoxChild
-			this.yimage1 = new global::Gamma.GtkWidgets.yImage();
-			this.yimage1.Name = "yimage1";
-			this.yimage1.Pixbuf = global::Stetic.IconLoader.LoadIcon(this, "gtk-find", global::Gtk.IconSize.Menu);
-			this.hbox2.Add(this.yimage1);
-			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.yimage1]));
-			w4.Position = 0;
+			this.ycheckbuttonChooseAll = new global::Gamma.GtkWidgets.yButton();
+			this.ycheckbuttonChooseAll.CanFocus = true;
+			this.ycheckbuttonChooseAll.Name = "ycheckbuttonChooseAll";
+			this.ycheckbuttonChooseAll.UseUnderline = true;
+			global::Gtk.Image w1 = new global::Gtk.Image();
+			w1.Pixbuf = global::Gdk.Pixbuf.LoadFromResource("Workwear.icon.buttons.select-all.png");
+			this.ycheckbuttonChooseAll.Image = w1;
+			this.hbox2.Add(this.ycheckbuttonChooseAll);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.ycheckbuttonChooseAll]));
+			w2.Position = 0;
+			w2.Expand = false;
+			w2.Fill = false;
+			// Container child hbox2.Gtk.Box+BoxChild
+			this.ycheckbuttonUnChooseAll = new global::Gamma.GtkWidgets.yButton();
+			this.ycheckbuttonUnChooseAll.CanFocus = true;
+			this.ycheckbuttonUnChooseAll.Name = "ycheckbuttonUnChooseAll";
+			this.ycheckbuttonUnChooseAll.UseUnderline = true;
+			global::Gtk.Image w3 = new global::Gtk.Image();
+			w3.Pixbuf = global::Gdk.Pixbuf.LoadFromResource("Workwear.icon.buttons.unselect-all.png");
+			this.ycheckbuttonUnChooseAll.Image = w3;
+			this.hbox2.Add(this.ycheckbuttonUnChooseAll);
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.ycheckbuttonUnChooseAll]));
+			w4.Position = 1;
 			w4.Expand = false;
 			w4.Fill = false;
 			// Container child hbox2.Gtk.Box+BoxChild
@@ -84,12 +68,21 @@ namespace Workwear.ReportParameters.Views
 			this.yentrySearch.InvisibleChar = '•';
 			this.hbox2.Add(this.yentrySearch);
 			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.yentrySearch]));
-			w5.Position = 1;
-			this.vbox1.Add(this.hbox2);
-			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbox2]));
-			w6.Position = 1;
+			w5.Position = 2;
+			// Container child hbox2.Gtk.Box+BoxChild
+			this.yimage1 = new global::Gamma.GtkWidgets.yImage();
+			this.yimage1.Name = "yimage1";
+			this.yimage1.Pixbuf = global::Stetic.IconLoader.LoadIcon(this, "gtk-find", global::Gtk.IconSize.Menu);
+			this.hbox2.Add(this.yimage1);
+			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.yimage1]));
+			w6.Position = 3;
 			w6.Expand = false;
 			w6.Fill = false;
+			this.vbox1.Add(this.hbox2);
+			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbox2]));
+			w7.Position = 0;
+			w7.Expand = false;
+			w7.Fill = false;
 			// Container child vbox1.Gtk.Box+BoxChild
 			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow();
 			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
@@ -101,8 +94,8 @@ namespace Workwear.ReportParameters.Views
 			this.ytreeChoiseSubdivision.Name = "ytreeChoiseSubdivision";
 			this.GtkScrolledWindow.Add(this.ytreeChoiseSubdivision);
 			this.vbox1.Add(this.GtkScrolledWindow);
-			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.GtkScrolledWindow]));
-			w8.Position = 2;
+			global::Gtk.Box.BoxChild w9 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.GtkScrolledWindow]));
+			w9.Position = 1;
 			this.Add(this.vbox1);
 			if ((this.Child != null))
 			{
