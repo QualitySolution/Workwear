874be18c94e6bebb94bf237ae37e5acf2d820197
Gankov <gav@qsolution.ru>
2023-12-28 19:05:13 +0300

    Исправил ложное срабатывание оптимистичной проверки на изменения.

--- Workwear.Desktop/HibernateMapping/Company/EmployeeCardMap.cs
+++ Workwear.Desktop/HibernateMapping/Company/EmployeeCardMap.cs
@@ -14,8 +14,7 @@ namespace Workwear.HibernateMapping.Company
 			else
 				Id(x => x.Id).Column("id").GeneratedBy.Native();
 			
-			OptimisticLock.Version();
-			Version(x => x.LastUpdate).Column("last_update");
+			Version(x => x.LastUpdate).Column("last_update").Generated.Always();
 
 			Map(x => x.CardNumber).Column("card_number");
 			Map(x => x.PersonnelNumber).Column("personnel_number");
