5867adce92eb065a6663ebc951248f1e7d712cab
Gankov <gav@qsolution.ru>
2024-02-06 17:01:22 +0300

    Добавил поле ласт апдейт в операцию выдачи пользователю.

--- Workwear.Sql/Scripts/2.8.15.sql
+++ Workwear.Sql/Scripts/2.8.15.sql
@@ -1,3 +1,4 @@
+-- В документ постомата добавлена ссылка на сотрудника
 alter table postomat_document_items
 	add employee_id int UNSIGNED NOT NULL after last_update;
 
@@ -5,9 +6,15 @@ alter table postomat_document_items
 	add constraint fk_employee_id
 		foreign key (employee_id) references wear_cards (id);
 
+-- В заявку на стрику добавлена ссылка на сотрудника.
 alter table clothing_service_claim
 	add employee_id int UNSIGNED NOT NULL after barcode_id;
 	
 alter table clothing_service_claim
 	add constraint fk_employee_id
 		foreign key (employee_id) references wear_cards (id);
+
+-- Добавлено время изменения в операции
+
+ALTER TABLE `operation_issued_by_employee` ADD `last_update` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `operation_time`;
+ALTER TABLE `operation_issued_by_employee` ADD INDEX(`last_update`);
