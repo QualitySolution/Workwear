ccf32d29efa617508f26875ce9308edc48bc6505
Иван <space@qsolution.ru>
2024-03-04 17:35:35 +0300

    Правки вьюмодели

--- Workwear/ReportParameters/ViewModels/NotIssuedSheetSummaryViewModel.cs
+++ Workwear/ReportParameters/ViewModels/NotIssuedSheetSummaryViewModel.cs
@@ -33,7 +33,7 @@ namespace workwear.ReportParameters.ViewModels
 		{
 			this.featuresService = featuresService ?? throw new ArgumentNullException(nameof(featuresService));
 
-			Title = "Справка по невыданному (Суммарно)";
+			Title = $"Справка по невыданному (Суммарно)";
 			UoW = uowFactory.CreateWithoutRoot();
 
 			var builder = new CommonEEVMBuilderFactory(rdlViewerViewModel, UoW, navigation, autofacScope);
@@ -88,16 +88,18 @@ namespace workwear.ReportParameters.ViewModels
 		private NotIssuedSheetSummaryReportType reportType;
 		public virtual NotIssuedSheetSummaryReportType ReportType {
 			get => reportType;
-			set => SetField(ref reportType, value);
+			set {
+				SetField(ref reportType, value);
+			}
 		}
-		
+
 		private DateTime? reportDate = DateTime.Today;
 		[PropertyChangedAlso(nameof(SensetiveLoad))]
 		public virtual DateTime? ReportDate {
 			get => reportDate;
 			set => SetField(ref reportDate, value);
 		}
-		
+
 		private DateTime? excludeBefore;
 		public virtual DateTime? ExcludeBefore {
 			get => excludeBefore;
@@ -165,7 +167,8 @@ namespace workwear.ReportParameters.ViewModels
 		public bool VisibleCondition => featuresService.Available(WorkwearFeature.ConditionNorm);
 		public bool VisibleChoiceEmployeeGroup => featuresService.Available(WorkwearFeature.EmployeeGroups);
 		public bool SensetiveLoad => ReportDate != null && !ChoiceProtectionToolsViewModel.AllUnSelected;
-		public object StockElementsSensetive => warehouse.Id != -2;
+		public bool VisibleShowEmployees => ReportType == NotIssuedSheetSummaryReportType.Flat;
+		public bool StockElementsSensetive => warehouse.Id != -2;
 		
 		private void ChoiceViewModelOnPropertyChanged(object sender, PropertyChangedEventArgs e) {
 			if(nameof(ChoiceProtectionToolsViewModel.AllUnSelected) == e.PropertyName)
