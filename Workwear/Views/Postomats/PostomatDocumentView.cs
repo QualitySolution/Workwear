c86d7cb2172354338dc7d9b4d52205f3e68fbc6a
Mikhail Pokrovskiy <pokrovskiy-2004@mail.ru>
2024-04-18 17:47:09 +0300

    Добавлена печать наклеек для выдачи через постомат

--- Workwear/Views/Postomats/PostomatDocumentView.cs
+++ Workwear/Views/Postomats/PostomatDocumentView.cs
@@ -48,13 +48,14 @@ namespace Workwear.Views.Postomats {
 				.Finish();
 			treeItems.Binding.AddBinding(Entity, e => e.Items, w => w.ItemsDataSource).InitializeFromSource();
 			treeItems.Selection.Changed += SelectionOnChanged;
+
+			enumPrint.ItemsEnum = typeof(PostomatDocumentViewModel.PostomatPrintType);
 			
 			SetEditableWindow();
 			
 			buttonDel.Clicked += (sender, args) => ViewModel.RemoveItem(treeItems.GetSelectedObject<PostomatDocumentItem>());
 			buttonAdd.Binding.AddBinding(ViewModel, v => v.CanAddItem, w => w.Sensitive).InitializeFromSource();
 			buttonAdd.Clicked += (sender, args) => ViewModel.ReturnFromService();
-			buttonPrint.Clicked += OnButtonPrintClicked;
 		}
 
 		private void SelectionOnChanged(object sender, EventArgs e) {
@@ -69,8 +70,9 @@ namespace Workwear.Views.Postomats {
 							ydateDoc.Sensitive = ViewModel.CanEdit;
 		}
 
-		private void OnButtonPrintClicked(object sender, EventArgs e) {
-			ViewModel.Print();
+		protected void OnEnumPrintClicked(object sender, QSOrmProject.EnumItemClickedEventArgs e) 
+		{
+			ViewModel.Print((PostomatDocumentViewModel.PostomatPrintType)e.ItemEnum);
 		}
 	}
 }
