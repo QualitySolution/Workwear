1347d158d71ce6d25a2bcf48803bbfd4e91b34a8
Иван <space@qsolution.ru>
2024-06-07 18:34:59 +0300

    Добавил маркер "Архивная в должность"

--- Workwear.Sql/Scripts/2.8.19.sql
+++ Workwear.Sql/Scripts/2.8.19.sql
@@ -1,2 +1,5 @@
 ﻿alter table `postomat_document_items`
 	modify column `cell_number` varchar(10) null default null
+
+alter table posts
+	add archival TINYINT(1) not null DEFAULT 0 after cost_center_id;
