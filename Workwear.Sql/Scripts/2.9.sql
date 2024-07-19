f953caa88d8628abf72e9a798ffa977038559e29
Kate <ekaterinatimireva1508@gmail.com>
2024-07-19 22:12:47 +0300

    Изменение наименований таблиц и ключей в new_empty.sql и добавление всех изменений в 2.9.sql

--- Workwear.Sql/Scripts/2.9.sql
+++ Workwear.Sql/Scripts/2.9.sql
@@ -78,3 +78,481 @@ DELETE FROM `item_types` WHERE category = 'property';
 alter table item_types 
 	drop column category,
 	drop column norm_life;
+
+
+-- переименование таблиц: objects в subdivisions, wear_cards в employees и таблички связи с wear_cards , 
+-- norms_professions в norms_posts, переименование ключей, индексов
+	    
+alter table clothing_service_claim
+drop foreign key fk_clothing_service_claim_employee_id;
+
+alter table departments
+drop foreign key fk_departaments_1;
+
+alter table employee_group_items
+	add constraint employee_groups_items_unique
+		unique (employee_id, employee_group_id);
+
+alter table employee_group_items
+drop key wear_card_groups_items_unique;
+
+alter table employee_group_items
+drop foreign key foreign_key_employee_groups_items_employees;
+
+alter table issuance_sheet
+drop foreign key fk_issuance_sheet_2;
+
+alter table issuance_sheet
+drop foreign key fk_issuance_sheet_8;
+
+alter table issuance_sheet_items
+drop foreign key fk_issuance_sheet_items_2;
+
+alter table leaders
+drop foreign key fk_leaders_1;
+
+create table norms_posts
+(
+	id      int unsigned auto_increment
+		primary key,
+	norm_id int unsigned not null,
+	post_id int unsigned not null,
+	constraint fk_norms_posts_1
+		foreign key (norm_id) references norms (id)
+			on update cascade on delete cascade,
+	constraint fk_norms_posts_2
+		foreign key (post_id) references posts (id)
+			on delete restrict
+			on update cascade
+);
+
+create index fk_norms_posts_1_idx
+	on norms_posts (norm_id);
+
+create index fk_norms_posts_2_idx
+	on norms_posts (post_id);
+
+drop table norms_professions;
+
+alter table operation_issued_by_employee
+drop foreign key fk_operation_issued_by_employee_1;
+
+alter table postomat_document_items
+drop foreign key fk_postomat_document_items_employee_id;
+
+alter table postomat_document_withdraw_items
+drop foreign key fk_postomat_document_withdraw_items_employee_id;
+
+alter table posts
+drop foreign key fk_posts_subdivision;
+
+alter table stock_collective_expense
+drop foreign key fk_stock_collective_expense_3;
+
+alter table stock_collective_expense_detail
+drop foreign key fk_stock_collective_expense_detail_6;
+
+alter table stock_expense
+	add employee_id int unsigned null default null after warehouse_id;
+
+create index fk_stock_expense_employee_idx
+	on stock_expense (employee_id);
+
+
+alter table stock_expense
+	drop foreign key fk_stock_expense_wear_card;
+
+drop index fk_stock_expense_wear_card_idx on stock_expense;
+
+
+alter table stock_expense
+drop column wear_card_id;
+
+alter table stock_income
+	add employee_id int unsigned null default null after warehouse_id;
+
+create index fk_stock_income_employee_idx
+	on stock_income (employee_id);
+
+
+alter table stock_income
+	drop foreign key fk_stock_income_wear_card;
+
+drop index fk_stock_income_wear_card_idx on stock_income;
+
+
+alter table stock_income
+drop column wear_card_id;
+
+create table subdivisions
+(
+	id                    int unsigned auto_increment
+		primary key,
+	code                  varchar(20)  null default null,
+	address               text         null default null,
+	name                  varchar(240) not null,
+	warehouse_id          int unsigned null default null,
+	parent_subdivision_id int unsigned null default null,
+	constraint fk_subdivisions_1
+		foreign key (warehouse_id) references warehouse (id)
+			on delete no action
+			on update no action ,
+	constraint fk_subdivisions_2
+		foreign key (parent_subdivision_id) references subdivisions (id)
+			on delete set null
+			on update no action
+)
+	collate = utf8mb3_general_ci;
+
+alter table departments
+	add constraint fk_departaments_1
+		foreign key (subdivision_id) references subdivisions (id)
+			on update cascade on delete set null;
+
+create table employees
+(
+	id                      int unsigned auto_increment
+		primary key,
+	last_update             timestamp  default current_timestamp() not null on update current_timestamp(),
+	card_number             varchar(15)                            null default null,
+	personnel_number        varchar(15)                            null default null,
+	last_name               varchar(20)                            null,
+	first_name              varchar(20)                            null,
+	patronymic_name         varchar(20)                            null,
+	card_key                varchar(16)                            null default null,
+	subdivision_id          int unsigned                           null default null,
+	department_id           int unsigned                           null default null,
+	hire_date               date                                   null default null,
+	change_of_position_date date                                   null default null,
+	dismiss_date            date                                   null default null,
+	post_id                 int unsigned                           null default null,
+	leader_id               int unsigned                           null default null,
+	sex                     enum ('F', 'M')                        null default null,
+	birth_date              date                                   null default null,
+	user_id                 int unsigned                           null default null,
+	phone_number            varchar(16)                            null default null,
+	lk_registered           tinyint(1) default 0                   not null default 0,
+	email                   text                                   null default null,
+	photo                   mediumblob                             null default null,
+	comment                 text                                   null default null,
+	constraint card_key_UNIQUE
+		unique (card_key),
+	constraint card_number_UNIQUE
+		unique (card_number),
+	constraint fk_employees_department
+		foreign key (department_id) references departments (id)
+			on update cascade on delete set null,
+	constraint fk_employees_leader
+		foreign key (leader_id) references leaders (id)
+			on update cascade on delete set null,
+	constraint fk_employees_post
+		foreign key (post_id) references posts (id)
+			on update cascade on delete set null,
+	constraint fk_employees_subdivision
+		foreign key (subdivision_id) references subdivisions (id)
+			on update cascade on delete set null,
+	constraint fk_employees_user
+		foreign key (user_id) references users (id)
+			on update cascade on delete set null
+);
+
+alter table clothing_service_claim
+	add constraint fk_clothing_service_claim_employee_id
+		foreign key (employee_id) references employees (id);
+
+create table employee_cards_item
+(
+	id                    int unsigned auto_increment
+		primary key,
+	employee_id           int unsigned not null,
+	protection_tools_id   int unsigned not null,
+	norm_item_id          int unsigned null,
+	created               date         null,
+	next_issue            date         null,
+	next_issue_annotation varchar(240) null default null,
+	constraint fk_employee_cards_item_2
+		foreign key (protection_tools_id) references protection_tools (id)
+			on delete restrict
+			on update cascade,
+	constraint fk_employee_cards_item_3
+		foreign key (norm_item_id) references norms_item (id)
+			on delete restrict
+			on update cascade,
+	constraint fk_employees_item_1
+		foreign key (employee_id) references employees (id)
+			on delete restrict
+			on update cascade
+);
+
+create index fk_employee_cards_item_2_idx
+	on employee_cards_item (protection_tools_id);
+
+create index fk_employee_cards_item_3_idx
+	on employee_cards_item (norm_item_id);
+
+create index fk_employees_item_1_idx
+	on employee_cards_item (employee_id);
+
+create index index_employee_cards_item_next_issue
+	on employee_cards_item (next_issue);
+
+alter table employee_group_items
+	add constraint foreign_key_employee_groups_items_employees
+		foreign key (employee_id) references employees (id)
+			on update cascade on delete cascade;
+
+create index fk_employees_department_idx
+	on employees (department_id);
+
+create index fk_employees_leader_idx
+	on employees (leader_id);
+
+create index fk_employees_post_idx
+	on employees (post_id);
+
+create index fk_employees_subdivision_idx
+	on employees (subdivision_id);
+
+create index fk_employees_user_idx
+	on employees (user_id);
+
+create index index_employees_dismiss_date
+	on employees (dismiss_date);
+
+create index index_employees_first_name
+	on employees (first_name);
+
+create index index_employees_last_name
+	on employees (last_name);
+
+create index index_employees_patronymic_name
+	on employees (patronymic_name);
+
+create index index_employees_personal_number
+	on employees (personnel_number);
+
+create index index_employees_phone_number
+	on employees (phone_number);
+
+create index last_update
+	on employees (last_update);
+
+create table employees_cost_allocation
+(
+	id             int unsigned auto_increment
+		primary key,
+	employee_id    int unsigned                        not null,
+	cost_center_id int unsigned                        not null,
+	percent        decimal(3, 2) unsigned default 1.00 not null,
+	constraint employees_cost_allocation_ibfk_1
+		foreign key (cost_center_id) references cost_center (id)
+			on delete restrict
+			on update cascade,
+	constraint employees_cost_allocation_ibfk_2
+		foreign key (employee_id) references employees (id)
+			on update cascade on delete cascade
+);
+
+create table employees_norms
+(
+	id          int unsigned auto_increment
+		primary key,
+	employee_id int unsigned not null,
+	norm_id     int unsigned not null,
+	constraint fk_employees_norms_1
+		foreign key (employee_id) references employees (id)
+			on update cascade on delete cascade,
+	constraint fk_employees_norms_2
+		foreign key (norm_id) references norms (id)
+			on update cascade on delete cascade
+);
+
+create index fk_employees_norms_1_idx
+	on employees_norms (employee_id);
+
+create index fk_employees_norms_2_idx
+	on employees_norms (norm_id);
+
+create table employees_sizes
+(
+	id           int unsigned auto_increment
+		primary key,
+	employee_id  int unsigned not null comment 'Сотрудник для которого установлен размер',
+	size_type_id int unsigned not null comment 'Тип размера, не может быть установлено несколько размеров одного типа одному сотруднику',
+	size_id      int unsigned not null,
+	constraint employees_sizes_unique
+		unique using btree(employee_id, size_type_id),
+	constraint fk_employees_sizes_1
+		foreign key (employee_id) references employees (id)
+			on update cascade on delete cascade,
+	constraint fk_employees_sizes_2
+		foreign key (size_type_id) references size_types (id)
+			on update cascade on delete cascade,
+	constraint fk_employees_sizes_3
+		foreign key (size_id) references sizes (id)
+			on update cascade on delete cascade
+);
+
+create index fk_employees_sizes_1_idx
+	on employees_sizes (employee_id);
+
+create index fk_employees_sizes_2_idx
+	on employees_sizes (size_type_id);
+
+create index fk_employees_sizes_3_idx
+	on employees_sizes (size_id);
+
+create table employees_vacations
+(
+	id               int unsigned auto_increment
+		primary key,
+	employee_id      int unsigned not null,
+	vacation_type_id int unsigned not null,
+	begin_date       date         not null,
+	end_date         date         not null,
+	comment          text         null default null,
+	constraint fk_employees_vacations_1
+		foreign key (employee_id) references employees (id)
+			on update cascade on delete cascade,
+	constraint fk_employees_vacations_2
+		foreign key (vacation_type_id) references vacation_type (id)
+			on delete restrict
+			on update cascade
+);
+
+create index fk_employees_vacations_1_idx
+	on employees_vacations (employee_id);
+
+create index fk_employees_vacations_2_idx
+	on employees_vacations (vacation_type_id);
+
+alter table issuance_sheet
+	add constraint fk_issuance_sheet_2
+		foreign key (subdivision_id) references subdivisions (id)
+			on delete no action
+			on update cascade;
+
+alter table issuance_sheet
+	add constraint fk_issuance_sheet_8
+		foreign key (transfer_agent_id) references employees (id)
+			on delete no action
+			on update cascade;
+
+alter table issuance_sheet_items
+	add constraint fk_issuance_sheet_items_2
+		foreign key (employee_id) references employees (id)
+			on delete no action
+			on update cascade;
+
+alter table leaders
+	add constraint fk_leaders_1
+		foreign key (employee_id) references employees (id)
+			on delete no action
+			on update cascade;
+
+alter table operation_issued_by_employee
+	add constraint fk_operation_issued_by_employee_1
+		foreign key (employee_id) references employees (id)
+			on delete restrict
+			on update cascade;
+
+alter table postomat_document_items
+	add constraint fk_postomat_document_items_employee_id
+		foreign key (employee_id) references employees (id);
+
+alter table postomat_document_withdraw_items
+	add constraint fk_postomat_document_withdraw_items_employee_id
+		foreign key (employee_id) references employees (id);
+
+alter table posts
+	add constraint fk_posts_subdivision
+		foreign key (subdivision_id) references subdivisions (id)
+			on update cascade on delete set null;
+
+alter table stock_collective_expense
+	add constraint fk_stock_collective_expense_3
+		foreign key (transfer_agent_id) references employees (id)
+			on delete restrict
+			on update cascade;
+
+alter table stock_collective_expense_detail
+	add constraint fk_stock_collective_expense_detail_6
+		foreign key (employee_id) references employees (id)
+			on delete no action
+			on update cascade;
+
+alter table stock_expense
+	add constraint fk_stock_expense_employee
+		foreign key (employee_id) references employees (id)
+			on delete restrict
+			on update cascade;
+
+alter table stock_income
+	add constraint fk_stock_income_employee
+		foreign key (employee_id) references employees (id)
+			on delete restrict
+			on update cascade;
+
+create index fk_subdivisions_1_idx
+	on subdivisions (warehouse_id);
+
+create index fk_subdivisions_2_idx
+	on subdivisions (parent_subdivision_id);
+
+create index index_subdivisions_code
+	on subdivisions (code);
+
+drop table wear_cards_cost_allocation;
+
+drop table wear_cards_item;
+
+drop table wear_cards_norms;
+
+drop table wear_cards_sizes;
+
+drop table wear_cards_vacations;
+
+drop table wear_cards;
+
+drop table objects;
+
+drop function count_issue;
+
+create
+definer = `t.ekaterina`@`%` function count_issue(amount int unsigned, norm_period int unsigned, next_issue date,
+                                                     begin_date date, end_date date, begin_Issue_Period int,
+                                                     end_Issue_Period int) returns int unsigned
+    comment 'Функция рассчитывает количество необходимое к выдачи.'
+    deterministic
+    no sql
+BEGIN
+DECLARE issue_count INT;
+
+IF norm_period <= 0 THEN RETURN 0; END IF;
+IF next_issue IS NULL THEN RETURN 0; END IF;
+
+SET issue_count = 0;
+
+WHILE next_issue <= end_date DO
+    IF next_issue >= begin_date THEN 
+    	SET issue_count = issue_count + amount;
+END IF;
+  SET next_issue = DATE_ADD(next_issue, INTERVAL norm_period MONTH);
+	IF begin_Issue_Period IS NOT NULL THEN
+		IF begin_Issue_Period < end_Issue_Period THEN
+			IF MONTH(next_issue) BETWEEN begin_Issue_Period AND end_Issue_Period THEN 
+             SET next_issue = DATE_ADD(CONCAT(YEAR(next_issue) , '-', end_Issue_Period, '-01'), INTERVAL 1 MONTH);
+END IF;
+ELSE
+			IF MONTH(next_issue) BETWEEN begin_Issue_Period AND 12 THEN
+				SET next_issue = DATE_ADD(CONCAT(YEAR(next_issue), '-', end_Issue_Period, '-01'), INTERVAL '1T1' YEAR_MONTH);
+			ELSEIF MONTH(next_issue) BETWEEN 1 AND end_Issue_Period THEN
+				SET next_issue = DATE_ADD(CONCAT(YEAR(next_issue), '-', end_Issue_Period, '-01'), INTERVAL 1 MONTH);
+END IF;
+END IF;
+END IF;
+END WHILE;
+RETURN issue_count;
+END;
+
+
