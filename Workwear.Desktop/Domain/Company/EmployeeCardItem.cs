685388931526d0b4bc07346e9973936f86e20ddd
Иван <space@qsolution.ru>
2024-08-27 15:40:40 +0300

    BestChoiceInStock не возвращает null

--- Workwear.Desktop/Domain/Company/EmployeeCardItem.cs
+++ Workwear.Desktop/Domain/Company/EmployeeCardItem.cs
@@ -132,7 +132,7 @@ namespace Workwear.Domain.Company
 		}
 		public virtual IEnumerable<StockBalance> BestChoiceInStock {
 			get {
-				var bestChoice = InStock.Where(x => x.Amount > 0).ToList();
+				var bestChoice = InStock?.Where(x => x.Amount > 0).ToList() ?? new List<StockBalance>();
 				bestChoice.Sort(new BestChoiceInStockComparer(ProtectionTools));
 				return bestChoice;
 			}
