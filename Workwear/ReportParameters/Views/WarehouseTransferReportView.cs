31e3b9794cb1b2bd849d98e7002e54cecd8c8ada
Kate <ekaterinatimireva1508@gmail.com>
2024-10-31 10:56:27 +0300

    Добавила действия во View и ViewModel.

--- Workwear/ReportParameters/Views/WarehouseTransferReportView.cs
+++ Workwear/ReportParameters/Views/WarehouseTransferReportView.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using QS.Views;
+using Workwear.ReportParameters.ViewModels;
+
 namespace Workwear.ReportParameters.Views {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class WarehouseTransferReportView : Gtk.Bin {
-		public WarehouseTransferReportView() {
+	public partial class WarehouseTransferReportView : ViewBase<WarehouseTransferReportViewModel> {
+		public WarehouseTransferReportView(WarehouseTransferReportViewModel viewModel): base(viewModel) {
 			this.Build();
+			ydateperiodpicker.Binding.AddSource(ViewModel)
+				.AddBinding(w=>w.StartDate, v=>v.StartDateOrNull)
+				.AddBinding(w=>w.EndDate, v=>v.EndDateOrNull)
+				.InitializeFromSource();
+			yenumcomboboxExpenseWarehouse.Binding.AddSource(ViewModel)
+				.AddBinding(w=>w.Warehouses,v=>v.ItemsEnum)
+				.AddBinding(w=>w.SelectWarehouse, v=>v.SelectedItem)
+				.InitializeFromSource();
+			yenumcomboboxReceiptWarehouse.Binding.AddSource(ViewModel)
+				.AddBinding(w=>w.Warehouses,v=>v.ItemsEnum)
+				.AddBinding(w=>w.SelectWarehouse, v=>v.SelectedItem)
+				.InitializeFromSource();
+			yenumcomboboxOwner.Binding.AddSource(ViewModel)
+				.AddBinding(w=>w.Owners,v=>v.ItemsEnum)
+				.AddBinding(w=>w.SelectOwner, v=>v.SelectedItem)
+				.InitializeFromSource();
+			buttonRun.Clicked += OnButtonRunClicked;
+			buttonRun.Binding.AddBinding(ViewModel,v=>v.SensetiveLoad,w=>w.Sensitive).InitializeFromSource();
+		}
+		protected void OnButtonRunClicked(object sender, EventArgs e) {
+			ViewModel.LoadReport();
 		}
 	}
 }
