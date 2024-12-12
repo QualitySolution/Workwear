926183703a584a3b544d56b9f134cc0f7772c4ff
Kate <ekaterinatimireva1508@gmail.com>
2024-12-12 11:26:21 +0300

    Добавила вьюшку и вьюмодель, добавила переименованную rdl в файл проекта.

---
+++ Workwear/ReportParameters/ViewModels/WearCardsReportViewModel.cs
@@ -0,0 +1,54 @@
+﻿using QS.Report.ViewModels;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Gamma.Utilities;
+using QS.Report;
+
+namespace Workwear.ReportParameters.ViewModels {
+	public class WearCardsReportViewModel: ReportParametersViewModelBase {
+		public WearCardsReportViewModel(RdlViewerViewModel rdlViewerViewModel) : base(rdlViewerViewModel) {
+			Title = "Список сотрудников";
+		}
+		public override string Identifier { 
+			get => ReportType.GetAttribute<ReportIdentifierAttribute>().Identifier;
+			set => throw new InvalidOperationException();
+		}
+
+		protected override Dictionary<string, object> Parameters => new Dictionary<string, object>() {
+			{"only_without_norms", OnlyWithoutNorms},
+			{"only_working", OnlyWorking},
+		};
+
+		private bool onlyWithoutNorms;
+		public virtual bool OnlyWithoutNorms {
+			get => onlyWithoutNorms;
+			set=>SetField(ref onlyWithoutNorms, value);
+		}
+
+		private bool onlyWorking;
+
+		public virtual bool OnlyWorking {
+			get => onlyWorking;
+			set=>SetField(ref onlyWorking, value);
+		}
+		private WearCardsReportType reportType;
+
+		public virtual WearCardsReportType ReportType {
+			get => reportType;
+			set {
+				SetField(ref reportType, value);
+        
+			}
+		}
+
+		public enum WearCardsReportType {
+			[ReportIdentifier("WearCardsReportFlat")]
+			[Display(Name="Только данные")]
+			Flat,
+			[ReportIdentifier("WearCardsReport")]
+			[Display(Name="Форматировано")]
+			Common
+		}
+	}
+}
---
+++ Workwear/ReportParameters/Views/WearCardsReportView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using QS.Views;
+using Workwear.ReportParameters.ViewModels;
+
+namespace Workwear.ReportParameters.Views {
+	public partial class WearCardsReportView : ViewBase<WearCardsReportViewModel> {
+		public WearCardsReportView(WearCardsReportViewModel viewModel) : base(viewModel) {
+			this.Build();
+			comboReportType.ItemsEnum = typeof(WearCardsReportViewModel.WearCardsReportType);
+			comboReportType.Binding.AddBinding(ViewModel, v=>v.ReportType, w=>w.SelectedItem).InitializeFromSource();
+			ycheckbuttonOnlyWithoutNorms.Binding.AddBinding(ViewModel, v=>v.OnlyWithoutNorms, w=>w.Active).InitializeFromSource();
+			ycheckbuttonOnlyWorking.Binding.AddBinding(ViewModel, v=>v.OnlyWorking, w=>w.Active).InitializeFromSource();
+		}
+		protected void OnYbuttonRunClicked(object sender, EventArgs e) {
+			ViewModel.LoadReport();
+		}
+	}
+}
