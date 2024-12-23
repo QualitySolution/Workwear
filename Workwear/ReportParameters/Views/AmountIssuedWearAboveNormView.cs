dc63a88169d9056f5d40b5c8847c193dd0e8520b
Kate <ekaterinatimireva1508@gmail.com>
2024-12-23 22:01:18 +0300

    Добавила вьюшку для отчета по выданному сверх нормы.

---
+++ Workwear/ReportParameters/Views/AmountIssuedWearAboveNormView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Workwear.ReportParameters.Views {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AmountIssuedWearAboveNormView : Gtk.Bin {
+		public AmountIssuedWearAboveNormView() {
+			this.Build();
+		}
+
+		protected void OnButtonPrintReportClicked(object sender, EventArgs e) {
+		}
+	}
+}
