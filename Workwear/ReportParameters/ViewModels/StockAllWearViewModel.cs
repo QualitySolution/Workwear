c5149b300c3ccd14310bd8d9fc5e58777a4ee64b
Kate <ekaterinatimireva1508@gmail.com>
2024-12-25 11:18:53 +0300

    Убрала старый виджет.

--- Workwear/ReportParameters/ViewModels/StockAllWearViewModel.cs
+++ Workwear/ReportParameters/ViewModels/StockAllWearViewModel.cs
@@ -2,38 +2,25 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using Autofac;
 using Gamma.Utilities;
 using Gamma.Widgets;
 using QS.DomainModel.UoW;
-using QS.Navigation;
 using QS.Report;
 using QS.Report.ViewModels;
-using QS.Services;
-using QS.ViewModels.Control.EEVM;
 using Workwear.Domain.Stock;
-using Workwear.Repository.Stock;
 using Workwear.Tools.Features;
 
 namespace Workwear.ReportParameters.ViewModels {
 	public class StockAllWearViewModel : ReportParametersViewModelBase{
 		public StockAllWearViewModel(RdlViewerViewModel rdlViewerViewModel,
 		IUnitOfWorkFactory unitOfWorkFactory,
-		INavigationManager navigation,
-		ILifetimeScope autofacScope,
-		StockRepository stockRepository,
 		FeaturesService featuresService)
 		: base(rdlViewerViewModel)
 		{
 			UoW = unitOfWorkFactory.CreateWithoutRoot();
-			var builder = new CommonEEVMBuilderFactory<StockAllWearViewModel>(rdlViewerViewModel, this, UoW, navigation, autofacScope);
 			this.featuresService = featuresService ?? throw new ArgumentNullException(nameof(featuresService));
-			
-			WarehouseEntry = builder.ForProperty(x => x.Warehouse).MakeByType().Finish();
-			//warehouse 
-			WarehouseEntry.Entity = stockRepository.GetDefaultWarehouse(UoW, featuresService, autofacScope.Resolve<IUserService>().CurrentUserId) 
-				?? UoW.GetAll<Warehouse>().First();
 			base.Title = "Складская ведомость";
+			//warehouse 
 			Warehouses=UoW.GetAll<Warehouse>().ToList();
 		}
 		
@@ -41,7 +28,7 @@ namespace Workwear.ReportParameters.ViewModels {
 		private readonly FeaturesService featuresService;
 		public bool VisibleWarehouse => featuresService.Available(WorkwearFeature.Warehouses);
 		public bool VisibleSumm => ReportType == StockAllWearReportType.Flat && featuresService.Available(WorkwearFeature.Selling);
-		public EntityEntryViewModel<Warehouse> WarehouseEntry;
+		
 		
 		protected override Dictionary<string, object> Parameters => new Dictionary<string, object> {
 			{"report_date", ReportDate },
