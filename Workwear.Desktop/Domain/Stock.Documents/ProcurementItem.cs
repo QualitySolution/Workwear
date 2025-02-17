b38b9e17c4ff50c1082889e6fc0c322f3e5f0cae
Kate <ekaterinatimireva1508@gmail.com>
2025-02-17 15:25:56 +0300

    Добавила доменный класс строки документа.

---
+++ Workwear.Desktop/Domain/Stock.Documents/ProcurementItem.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QS.BusinessCommon.Domain;
+using QS.DomainModel.Entity;
+using QS.HistoryLog;
+using Workwear.Domain.Sizes;
+
+namespace Workwear.Domain.Stock.Documents {
+	[Appellative (Gender = GrammaticalGender.Feminine,
+		NominativePlural = "строки предполагаемой поставки",
+		Nominative = "строка предполагаемой поставки",
+		Genitive = "строки предполагаемой поставки"
+	)]
+	[HistoryTrace]
+	public class ProcurementItem: IDomainObject 
+	{
+		#region Свойства
+		public virtual int Id { get; set; }
+		
+		private Procurement procurement;
+		
+		[Display(Name = "Документ")]
+		[IgnoreHistoryTrace]
+		public virtual Procurement Procurement {
+			get => procurement;
+			set {procurement = value;}
+		}
+		
+		Nomenclature nomenclature;
+		[Display (Name = "Номенклатура")]
+		public virtual Nomenclature Nomenclature {
+			get => nomenclature;
+			set { nomenclature = value; }
+		}
+		
+		[Display (Name = "Наименование")]
+		public virtual string ItemName {
+			get => nomenclature?.Name; 
+		}
+		
+		[Display(Name = "Тип Роста")]
+		public virtual SizeType HeightType {
+			get => nomenclature?.Type.HeightType;
+		}
+		
+		[Display(Name = "Тип размера одежды")]
+		public virtual SizeType WearSizeType {
+			get => nomenclature?.Type.SizeType;
+		}
+		
+		[Display(Name = "Единица измерения")]
+		public virtual MeasurementUnits Units {
+			get => nomenclature?.Type.Units; 
+		}
+		
+		private int amount;
+		[Display (Name = "Количество")]
+		[PropertyChangedAlso("Total")]
+		public virtual int Amount {
+			get => amount;
+			set {amount = value; }
+		}
+
+		private decimal cost;
+		[Display (Name = "Цена")]
+		[PropertyChangedAlso("Total")]
+		public virtual decimal Cost {
+			get => cost;
+			set { cost = value; }
+		}
+		
+		private Size wearSize;
+		[Display(Name = "Размер")]
+		public virtual Size WearSize {
+			get => wearSize;
+			set => wearSize = value;
+		}
+		
+		private Size height;
+		[Display(Name = "Рост одежды")]
+		public virtual Size Height {
+			get => height;
+			set => height = value;
+		}
+		
+		private string comment;
+		[Display(Name = "Комментарий")]
+		public virtual string Comment {
+			get =>comment;
+			set { comment = value; }
+		}
+		#endregion
+		
+		#region Расчетные
+
+		public virtual string Title => $"Закупка {Nomenclature?.Name} в количестве {Amount} {Nomenclature?.Type?.Units?.Name}";
+		public virtual decimal Total => Cost * Amount;
+
+		#endregion
+
+		public ProcurementItem(){ }
+
+		public ProcurementItem(Procurement procurement) {
+			this.procurement = procurement;
+		}
+	}
+}
