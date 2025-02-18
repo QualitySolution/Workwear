45c6c741458abd0679973e570c7641687ed8ec64
Kate <ekaterinatimireva1508@gmail.com>
2025-02-18 17:11:46 +0300

    Добавила вьюшку.

---
+++ Workwear/Views/Stock/ProcurementView.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Workwear.Views.Stock {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ProcurementView : Gtk.Bin {
+		public ProcurementView() {
+			this.Build();
+		}
+	}
+}
