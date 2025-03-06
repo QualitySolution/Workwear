87cadf4113fb4f3c0c3e96f28f3d11e6dd5c5245
Kate <ekaterinatimireva1508@gmail.com>
2025-03-06 15:38:54 +0300

    Правки в скриптах.

--- Workwear.Sql/Scripts/new_empty.sql
+++ Workwear.Sql/Scripts/new_empty.sql
@@ -2451,7 +2451,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('product_name', 'workwear');
-INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('version', '2.9');
+INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('version', '2.9.2');
 INSERT INTO `base_parameters` (`name`, `str_value`) VALUES ('DefaultAutoWriteoff', 'True');
 
 COMMIT;
