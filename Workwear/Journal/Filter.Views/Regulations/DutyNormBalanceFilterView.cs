bab3e8775a57e985e5b020893887a400f116a297
Kate <ekaterinatimireva1508@gmail.com>
2025-03-11 14:45:04 +0300

    Добавила фильтр для журнала.

---
+++ Workwear/Journal/Filter.Views/Regulations/DutyNormBalanceFilterView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using QS.Views;
+using Workwear.Journal.Filter.ViewModels.Regulations;
+
+namespace Workwear.Journal.Filter.Views.Regulations {
+	public partial class DutyNormBalanceFilterView : ViewBase<DutyNormBalanceFilterViewModel> {
+		public DutyNormBalanceFilterView(DutyNormBalanceFilterViewModel viewModel): base(viewModel) {
+			this.Build();
+			ConfigureDlg();
+		}
+
+		private void ConfigureDlg() {
+			datepicker.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm=>vm.Date, w=>w.Date)
+				.AddBinding(vm=>vm.DateSensitive, w=>w.Sensitive)
+				.InitializeFromSource();
+			yentryDutyNorm.Binding.AddBinding(ViewModel, vm=>vm.DutyNormSensitive, w=>w.Sensitive)
+				.InitializeFromSource();
+			yentrySubdivision.Binding.AddBinding(ViewModel, vm=>vm.SubdivisionSensitive, w=>w.Sensitive)
+				.InitializeFromSource();
+			yentryDutyNorm.ViewModel = ViewModel.DutyNormEntry;
+			yentrySubdivision.ViewModel=ViewModel.SubdivisionEntry;
+		}
+	}
+}
