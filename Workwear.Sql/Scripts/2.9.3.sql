7696b1a482121b397e14e74a32c0be788974d631
Kate <ekaterinatimireva1508@gmail.com>
2025-03-24 13:34:01 +0300

    Добавление операции списания с дежурной нормы в строки док-та списания.

---
+++ Workwear.Sql/Scripts/2.9.3.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE stock_write_off_detail
+	ADD COLUMN duty_norm_issue_operation_id int(10) unsigned NULL DEFAULT NULL AFTER warehouse_operation_id;
+ALTER TABLE stock_write_off_detail
+	ADD CONSTRAINT fk_stock_write_off_detail_duty_norm_issue_operation
+		FOREIGN KEY (duty_norm_issue_operation_id) REFERENCES operation_issued_by_duty_norm(id)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION;
+CREATE INDEX fk_stock_write_off_detail_duty_norm_issue_operation_idx
+	ON stock_write_off_detail (duty_norm_issue_operation_id ASC);
