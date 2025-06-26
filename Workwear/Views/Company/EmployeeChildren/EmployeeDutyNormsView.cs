c793f18971f1bc3117b4d1cce5687141fcf1712e
Kate <ekaterinatimireva1508@gmail.com>
2025-06-26 12:57:06 +0300

    Добавление вьюшки

---
+++ Workwear/Views/Company/EmployeeChildren/EmployeeDutyNormsView.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Workwear.Views.Company.EmployeeChildren {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EmployeeDutyNormsView : Gtk.Bin {
+		public EmployeeDutyNormsView() {
+			this.Build();
+		}
+	}
+}
