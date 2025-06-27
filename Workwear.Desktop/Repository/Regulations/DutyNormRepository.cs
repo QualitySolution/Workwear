7fdaaf7ef5414555537643ee5eae2b671394ea9d
Kate <ekaterinatimireva1508@gmail.com>
2025-06-27 13:05:02 +0300

    Добавление запроса в репозиторий дежурных норм.
    Для получения всех дежурных норм с выбранным ответственным сотрудником.

--- Workwear.Desktop/Repository/Regulations/DutyNormRepository.cs
+++ Workwear.Desktop/Repository/Regulations/DutyNormRepository.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using NHibernate;
 using NHibernate.Criterion;
 using QS.DomainModel.UoW;
+using Workwear.Domain.Company;
 using Workwear.Domain.Operations;
 
 namespace Workwear.Repository.Regulations {
 	public class DutyNormRepository {
+		private readonly UnitOfWorkProvider unitOfWorkProvider;
+		private IUnitOfWork repoUow;
+		public IUnitOfWork RepoUow {
+			get => repoUow ?? unitOfWorkProvider.UoW;
+			set => repoUow = value;
+		}
 		public Dictionary<int, int> CalculateWrittenOff(DutyNormIssueOperation[] operations, IUnitOfWork uow, DateTime? onDate = null) {
-			var wo = uow.Session.QueryOver<DutyNormIssueOperation>()
+			var wo = (uow ?? RepoUow).Session.QueryOver<DutyNormIssueOperation>()
 				.Where(o => o.IssuedOperation.Id
 					.IsIn(operations.Select(x => x.Id).ToArray()));
 			if(onDate != null)
@@ -17,6 +25,23 @@ namespace Workwear.Repository.Regulations {
 			return wo.List()
 				?.GroupBy(o => o.Id)
 				.ToDictionary(g => g.Key, g => g.Sum(o => o.Returned));
+		}
+
+		/// <summary>
+		/// Получаем все операции выдачи по дежурным нормам для выбранного ответственного сотрудника (отсортированы в порядке убывания).
+		/// </summary>
+		/// <returns></returns>
+		/// 
+		public virtual IList<DutyNormIssueOperation> AllDutyNormsForResponsibleEmployee(
+			EmployeeCard employee, 
+			Action<IQueryOver<DutyNormIssueOperation, DutyNormIssueOperation>> makeEager = null, IUnitOfWork uow = null)
+		{
+			var query = (uow ?? RepoUow).Session.QueryOver<DutyNormIssueOperation>()
+				.Where(o => o.DutyNorm.ResponsibleEmployee == employee);
+
+			makeEager?.Invoke(query);
+
+			return query.OrderBy(x => x.OperationTime).Desc.List();
 		}
 	}
 }
