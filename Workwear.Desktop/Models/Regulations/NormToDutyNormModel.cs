2ee1511429de8bf76695a073ae253f17d7e51519
Kate <ekaterinatimireva1508@gmail.com>
2025-08-01 17:48:51 +0300

    Правки в копировании строк нормы. Реализовала метод для копирования операций выдачи.

--- Workwear.Desktop/Models/Regulations/NormToDutyNormModel.cs
+++ Workwear.Desktop/Models/Regulations/NormToDutyNormModel.cs
@@ -1,5 +1,9 @@
-﻿using QS.DomainModel.UoW;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Criterion;
+using QS.DomainModel.UoW;
 using Workwear.Domain.Company;
+using Workwear.Domain.Operations;
 using Workwear.Domain.Operations.Graph;
 using Workwear.Domain.Regulations;
 
@@ -10,6 +14,7 @@ namespace Workwear.Models.Regulations {
 			using(var uow = UnitOfWorkFactory.CreateWithoutRoot("Копирование данных из нормы")) {
 				var norm = uow.GetById<Norm>(normId);
 				var employee = uow.GetById<EmployeeCard>(employeeId);
+				var itemIds = norm.Items.Select(i => i.Id).ToArray();
 				newDutyNorm.Name = norm.Name != null ? $"{norm.Name} ({employee.ShortName})" : $"Дежурная ({employee.ShortName})";
 				newDutyNorm.ResponsibleEmployee = employee;
 				newDutyNorm.DateFrom = norm.DateFrom;
@@ -19,14 +24,25 @@ namespace Workwear.Models.Regulations {
 				uow.Save(newDutyNorm);
 
 				foreach(var item in norm.Items) {
-					var dutyNormItem = CopyNormItems(newDutyNorm, item);
+					var employeeCardItem = uow.Session
+						.Query<EmployeeCardItem>()
+						.Where(x => x.EmployeeCard.Id == employeeId)
+						.FirstOrDefault(x => x.ActiveNormItem.Id == item.Id);
+					var dutyNormItem = CopyNormItems(newDutyNorm, item, employeeCardItem);
 					uow.Save(dutyNormItem);
 				}
+
+				var employeeIssueOperations = GetOperationsForEmployeeWithNormItem(employeeId, itemIds, uow);
+				foreach(var op in employeeIssueOperations) {
+					var dutyNormIssueOperation = СopyEmployeeIssueOperations(newDutyNorm, op);
+					uow.Save(dutyNormIssueOperation);
+				}
+				
 				uow.Commit();
 			}
 		}
 
-		public virtual DutyNormItem CopyNormItems(DutyNorm newDutyNorm, NormItem normItem) {
+		public virtual DutyNormItem CopyNormItems(DutyNorm newDutyNorm, NormItem normItem, EmployeeCardItem employeeCardItem) {
 			DutyNormItem newDutyNormItem = new DutyNormItem();
 			
 			newDutyNormItem.DutyNorm = newDutyNorm;
@@ -51,12 +67,32 @@ namespace Workwear.Models.Regulations {
 			newDutyNormItem.NormParagraph = normItem.NormParagraph;
 			newDutyNormItem.Comment = normItem.Comment;
 			newDutyNormItem.Graph = new IssueGraph();
+			newDutyNormItem.NextIssue = employeeCardItem.NextIssue;
 			
 			return newDutyNormItem;
 		}
 		
-		public virtual void СopyEmployeeIssueOperations (DutyNorm newDutyNorm, Norm norm) {
+		public virtual DutyNormIssueOperation СopyEmployeeIssueOperations (DutyNorm newDutyNorm, EmployeeIssueOperation issueOperation) {
+			DutyNormIssueOperation dutyNormIssueOperation = new DutyNormIssueOperation();
+
+			dutyNormIssueOperation.DutyNorm = newDutyNorm;
+			dutyNormIssueOperation.OperationTime = issueOperation.OperationTime;
+			dutyNormIssueOperation.Nomenclature = issueOperation.Nomenclature;
+			dutyNormIssueOperation.ProtectionTools = issueOperation.ProtectionTools;
+			dutyNormIssueOperation.WearPercent = issueOperation.WearPercent;
+			dutyNormIssueOperation.AutoWriteoffDate = issueOperation.AutoWriteoffDate;
+			dutyNormIssueOperation.Issued = issueOperation.Issued;
 			
+			return dutyNormIssueOperation;
+		}
+
+		public IList<EmployeeIssueOperation> GetOperationsForEmployeeWithNormItem(int employeeId, int[] normItemsIds, IUnitOfWork uow) {
+			var query = uow.Session.QueryOver<EmployeeIssueOperation>()
+				.Where(o => o.NormItem.Id.IsIn(normItemsIds))
+				.Where(o => o.Employee.Id == employeeId)
+				.Where(o => o.Issued > 0);
+			return query.List();
+
 		}
 	}
 }
