1cc04f0058ca8d4f129610551860d0ce712aa7b6
Kate <ekaterinatimireva1508@gmail.com>
2025-08-29 13:38:51 +0300

    Добавление вьюшки для заявки с вкладкой "Основное".

---
+++ Workwear/Views/Visits/IssuanceRequestView.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Workwear.Views.Visits {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class IssuanceRequestView : Gtk.Bin {
+		public IssuanceRequestView() {
+			this.Build();
+		}
+	}
+}
