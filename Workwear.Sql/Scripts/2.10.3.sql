b928ff78b23950bf9ec759856ea180bcf8f4cd72
Gankov <gav@qsolution.ru>
2025-09-04 10:01:17 +0300

    Исправление ошибок в скриптах работы с базой.

--- Workwear.Sql/Scripts/2.10.3.sql
+++ Workwear.Sql/Scripts/2.10.3.sql
@@ -1,4 +1,4 @@
--- Новое раписание работы склада
+-- Новое расписание работы склада
 create table days_schedule (
 	   id   	     	int unsigned auto_increment,
 	   day_of_week 		int unsigned not null,
@@ -6,7 +6,7 @@ create table days_schedule (
 	   end 		    	time not null,
 	   visit_interval   int unsigned not null,
 	   		constraint days_schedule
-		  	primary key (id),
+		  	primary key (id)
 );
 
 -- Создаем пропущенные индексы для снижения нагрузки на ЦП сервиса постоматов
--- Workwear.Sql/Scripts/new_empty.sql
+++ Workwear.Sql/Scripts/new_empty.sql
@@ -2493,6 +2493,7 @@ create table work_days
 		constraint work_days_pk
 		primary key (id)
 );
+
 create table days_schedule (
 	id   	     	int unsigned auto_increment,
 	day_of_week 	int unsigned not null,
@@ -2500,7 +2501,7 @@ create table days_schedule (
 	end 		    time not null,
 	visit_interval  int unsigned not null,
 		constraint days_schedule
-	   	primary key (id),
+	   	primary key (id)
 );
 -- -----------------------------------------------------
 -- Оказываемые услуги
