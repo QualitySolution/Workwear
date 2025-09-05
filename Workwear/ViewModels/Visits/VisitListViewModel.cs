88ba34d425aa2db84542dece94332c6a2a2f7ed7
Иван <space@qsolution.ru>
2025-09-05 12:01:23 +0300

    Вынес не зависящие от базы действия в модель

--- Workwear/ViewModels/Visits/VisitListViewModel.cs
+++ Workwear/ViewModels/Visits/VisitListViewModel.cs
@@ -12,6 +12,7 @@ using QS.ViewModels.Dialog;
 using Workwear.Domain.Company;
 using Workwear.Domain.Stock.Documents;
 using Workwear.Domain.Visits;
+using Workwear.Models.Visits;
 using Workwear.ViewModels.Stock;
 
 namespace Workwear.ViewModels.Visits {
@@ -31,17 +32,15 @@ namespace Workwear.ViewModels.Visits {
 			
 			Title = "Посещения склада";
 			
-			var allDaysSchedule = UoW.GetAll<DaySchedule>().ToList();
-			DaysSchedule = allDaysSchedule.Where(d => d.Date == null).ToList();
-			ExclusiveDays = allDaysSchedule.Where(d => d.Date != null).ToList();
-			
+			listModel = new VisitListModel(UoW.GetAll<DaySchedule>().ToList());
+
 			//Для обновления вюшки при действиях с документами
 			changeWatcher.BatchSubscribe(VisitChangeEvent).IfEntity<Expense>();
 			changeWatcher.BatchSubscribe(VisitChangeEvent).IfEntity<Return>();
 			changeWatcher.BatchSubscribe(VisitChangeEvent).IfEntity<Writeoff>();
 
 			loadData();
-			if(Items.Count == 0)
+			if(listModel.Items.Count == 0)
 				NextDay();
 		}
 		//TODO докумментация IDialogDocumentation
@@ -50,12 +49,10 @@ namespace Workwear.ViewModels.Visits {
 		
 		private readonly INavigationManager navigation;
 		private readonly IEntityChangeWatcher changeWatcher;
+		private VisitListModel listModel;
+		public SortedDictionary<DateTime, VisitListItem> Items => listModel.Items; 
 		public DateTime Day { get; set; } = DateTime.Now.Date;
 		public string PeriodString => Day.Date.ToShortDateString();
-		public List<DaySchedule> ExclusiveDays { get; }
-		public List<DaySchedule> DaysSchedule { get;}
-		//Коллекция отображаемых номерков
-		public SortedDictionary<DateTime, VisitListWidgetItem> Items { get; set; } = new SortedDictionary<DateTime, VisitListWidgetItem>();
 
 		//Для синхронизации с изменениями внесёнными в базу при открытом диалоге.
 		private void VisitChangeEvent(EntityChangeEvent[] changeevents) {
@@ -80,51 +77,20 @@ namespace Workwear.ViewModels.Visits {
 			foreach(var v in Items.Where(x => x.Value.Visit != null)) 
 				UoW.Session.Evict(v.Value.Visit);
 			Items.Clear();
-			
-			var visits = UoW.GetAll<Visit>() 
+
+			listModel.PutVisits(UoW.GetAll<Visit>()
 				.Where(x => x.VisitTime >= Day)
 				.Where(x => x.VisitTime < Day.AddDays(1))
 				.Fetch(x => x.Employee)
-				.ToList();
-			
-			foreach(var visit in visits) {
-				while(Items.ContainsKey(visit.VisitTime))
-					visit.VisitTime = visit.VisitTime.AddSeconds(1);
-				Items.Add(visit.VisitTime, new VisitListWidgetItem(visit));
-			}
+				.ToList()
+			);
 
-			if(IsWorkDay(Day))
-				foreach(var time in MakeSchedule(Day).Where(x => !Items.ContainsKey(x)))
-					Items.Add(time, new VisitListWidgetItem(time));
+			if(listModel.IsWorkDay(Day))
+				listModel.FillScheduleOfDay(Day);
 			
 			OnPropertyChanged(nameof(Items));
 		}
-		/// <summary>
-		/// Создаёт график. Список DataTime начла возможных записей.
-		/// </summary>
-		private List<DateTime> MakeSchedule(DateTime day) {
-			List<DateTime> result = new List<DateTime>();
-			List<DaySchedule> ScheduleList = (ExclusiveDays.Any(d => d.Date == day)
-				? ExclusiveDays.Where(d => d.Date == day)
-				: DaysSchedule.Where(d => d.DayOfWeak == (int)day.DayOfWeek % 7))
-				.ToList();
-			foreach(var schedule in ScheduleList) {
-				DateTime start = day.Date + (schedule.Start); 
-				DateTime end = day.Date + (schedule.End);
-			for(DateTime time = start; time < end; time = time.AddMinutes(schedule.Interval)) 
-				result.Add(time);
-			}
-			return result;
-		}
 
-		private bool IsWorkDay(DateTime day) {
-			if(ExclusiveDays.Any(d => d.Date == day))
-				return ExclusiveDays.Any(d => d.Date == day && d.IsWork);
-			   
-			if(DaysSchedule.Any(d => d.DayOfWeak == (int)day.DayOfWeek % 7))
-				return DaysSchedule.Any(d => d.DayOfWeak == (int)day.DayOfWeek % 7 && d.IsWork);
-			return false;
-		}
 		
 		public void AddExpance(EmployeeCard employee, Visit visit) {
 			navigation.OpenViewModel<ExpenseEmployeeViewModel, IEntityUoWBuilder, EmployeeCard, Visit>(null, EntityUoWBuilder.ForCreate(), employee, visit);
@@ -164,56 +130,12 @@ namespace Workwear.ViewModels.Visits {
 				OnPropertyChanged(nameof(PeriodString));
 		}
 
-		public void AddComment(VisitListWidgetItem item, string entryText) {
+		public void AddComment(VisitListItem item, string entryText) {
 			if(item?.Visit != null && item.Visit.Comment != entryText) {
 				item.Visit.Comment = entryText;
 				UoW.Save(item.Visit);
 				UoW.Commit();
 			}
 		}
-	}
-	
-	public class VisitListWidgetItem : PropertyChangedBase {
-	
-		public VisitListWidgetItem(DateTime visitTime) {
-			this.VisitTime = visitTime;
-		}
-		public VisitListWidgetItem(Visit visit) {
-			if(visit != null) {
-				this.Visit = visit;
-				this.VisitTime = visit.VisitTime;
-				this.Employee = visit.Employee;
-			}
-			else 
-				throw new NullReferenceException(nameof(visit));
-		}
-		
-		public DateTime VisitTime  { get; }
-		
-		public Visit Visit { get; }
-		public DateTime? CreateTime => Visit?.CreateDate;
-		public string Comment => Visit?.Comment;
-
-		public string DocumentsString => string.Join("\n", Documents.Select(x => x.label));
-
-		public List<(StockDocumentType doc, int id, string label)> Documents {
-			get {
-				var result = new List<(StockDocumentType, int, string)>();
-				if(Visit?.ExpenseDocuments != null) 
-					result.AddRange(Visit.ExpenseDocuments.Select(x => 
-						(StockDocumentType.ExpenseEmployeeDoc, x.Id, $"{(x.IssueDate != null ? "Выдача" : "Черновик")} {x.DocNumberText}" )));
-				if(Visit?.WriteoffDocuments != null)
-					result.AddRange(Visit.WriteoffDocuments.Select(x => 
-						(StockDocumentType.WriteoffDoc, x.Id,  $"Списание {x.DocNumberText}" )));
-				if(Visit?.ReturnDocuments != null) 
-					result.AddRange(Visit.ReturnDocuments.Select(x =>
-						(StockDocumentType.Return, x.Id, $"Возврат {x.DocNumberText}" ))); 
-				return result;
-			}
-		}
-		
-		public EmployeeCard Employee  { get; }
-		public string FIO => Employee?.FullName ?? "";
-		
 	}
 }
