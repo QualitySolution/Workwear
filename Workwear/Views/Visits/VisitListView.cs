72d3625cc2c52e9656570e0c7e6c56ab010e81ab
Kate <ekaterinatimireva1508@gmail.com>
2025-10-08 19:58:48 +0300

    Форматирование таблицы
    Добавлена колонка с кнопками-действиями, изменена индексация

--- Workwear/Views/Visits/VisitListView.cs
+++ Workwear/Views/Visits/VisitListView.cs
@@ -36,50 +36,56 @@ namespace Workwear.Views.Visits {
 			
 			//Шапка
 			var rows = ViewModel.Items.Count;
-			ItemListTable.Resize((uint)(rows + 3), 6);
+			ItemListTable.Resize((uint)(rows + 3), 7);
 
 			var label2 = new Label {Markup = "<b>Интервал</b>"};
-			ItemListTable.Attach(label2, 4, 5, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+			ItemListTable.Attach(label2, 0, 1, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 
 			var label3 = new Label {Markup = "<b>Сотрудник</b>"};
-			ItemListTable.Attach(label3, 6, 7, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+			ItemListTable.Attach(label3, 2, 3, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 
 			var label4 = new Label {Markup = "<b>Создан</b>"};
-			ItemListTable.Attach(label4, 8, 9, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+			ItemListTable.Attach(label4, 4, 5, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 			
 			var label5 = new Label {Markup = "<b>Документы</b>"};
-			ItemListTable.Attach(label5, 10, 13, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
-
-			var label6 = new Label {Markup = "<b>Комментарий</b>", Xalign = 0, };
-			ItemListTable.Attach(label6, 14, 15, 0, 1, AttachOptions.Expand, AttachOptions.Shrink, 0, 0);
-
-			//Границы таблицы (разделители)
-			ItemListTable.Attach(new VSeparator(), 5, 6, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
-			ItemListTable.Attach(new VSeparator(), 7, 8, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
-			ItemListTable.Attach(new VSeparator(), 9, 10, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
-			ItemListTable.Attach(new VSeparator(), 13, 14, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);			
+			ItemListTable.Attach(label5, 6, 9, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 			
-            ItemListTable.Attach(new HSeparator(), 0, 15, 1, 2);
-            ItemListTable.Attach(new HSeparator(), 0, 15, 3, 4);
+			var label6 = new Label { Markup = "<b>Действия</b>" };
+			ItemListTable.Attach(label6, 10, 11, 0, 1, AttachOptions.Shrink, AttachOptions.Shrink, 0 ,0);
+			
+			var label7 = new Label {Markup = "<b>Комментарий</b>", Xalign = 0, };
+			ItemListTable.Attach(label7, 12, 15, 0, 1, AttachOptions.Expand, AttachOptions.Shrink, 0, 0);
+			
+			//Границы таблицы (разделители)
+			ItemListTable.Attach(new VSeparator(), 1, 2, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
+			ItemListTable.Attach(new VSeparator(), 3, 4, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
+			ItemListTable.Attach(new VSeparator(), 5, 6, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
+			ItemListTable.Attach(new VSeparator(), 9, 10,0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 0 ,0);
+			ItemListTable.Attach(new VSeparator(), 11, 12, 0, (uint)(3 * rows + 3), AttachOptions.Shrink, AttachOptions.Fill, 2, 0);			
+			
+			
+           ItemListTable.Attach(new HSeparator(), 0, 15, 1, 2);
+           ItemListTable.Attach(new HSeparator(), 0, 15, 3, 4);
 	
             //Заполнение данными
-			uint i = 5;
+			uint i = 6;
 			foreach(var item in ViewModel.Items.Values) {
 				Label label;
 				Entry entry;
 				TextView textView;
 				Button button;
+				HBox buttonBox = new HBox(false, 2);
 				uint j = (uint) item.Documents.Count; //смещение многострочных элементов
 
 				label = new Label {LabelProp = item.VisitTime.ToShortTimeString()}; //Время
-                ItemListTable.Attach(label, 4, 5, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+                ItemListTable.Attach(label, 0, 1, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 
                 label = new Label {LabelProp = item.FIO}; //Сотрудник
-                ItemListTable.Attach(label, 6, 7, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+                ItemListTable.Attach(label, 2, 3, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 
                 if(item.CreateTime != null) {
 	                label = new Label { LabelProp = ((DateTime)item.CreateTime).ToShortDateString() }; //Дата создания
-	                ItemListTable.Attach(label, 8, 9, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+	                ItemListTable.Attach(label, 4, 5, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
                 }
                 
                 if(item.Documents.Count != 0) { //Документы
@@ -88,28 +94,53 @@ namespace Workwear.Views.Visits {
 		                button = new Button(); //открыть
 		                button.Label = doc.label;
 		                button.Clicked += (sender, args) =>  ViewModel.OpenDocument(doc.doc, doc.id);
-		                ItemListTable.Attach(button, 10, 11, i+n, i+n+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+		                ItemListTable.Attach(button, 6, 7, i+n, i+n+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
 		                n++;
 	                }
                 }
-
+                
                 if(item.Employee != null) {
 	                button = new Button(); //добавить
 	                button.Clicked += (sender, args) =>  ViewModel.AddExpance(item.Employee, item.Visit);
 	                Image w3 = new Image();
 	                w3.Pixbuf = Stetic.IconLoader.LoadIcon(this, "gtk-add", IconSize.Menu);
 	                button.Image = w3;
-	                ItemListTable.Attach(button, 12, 13, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
+	                ItemListTable.Attach(button, 8, 9, i, i+j+1, AttachOptions.Shrink, AttachOptions.Shrink, 0, 0);
                 
 	                if(!string.IsNullOrEmpty(item.DocumentsString))
-						ItemListTable.Attach(new VSeparator(), 11, 12, i, i+j+1, AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
+						ItemListTable.Attach(new VSeparator(), 7, 8, i, i+j+1, AttachOptions.Shrink, AttachOptions.Fill, 0, 0);
 				}
 
+				for (uint l =0; l < 4; l++) {
+					button = new Button();
+					Image img = new Image();
+					string name = String.Empty;
+					switch(l) {
+						case 0:
+							name = "gtk-media-play"; 
+							break;
+						case 1:
+							name = "gtk-ok";
+							break;
+						case 2:
+							name = "gtk-close";
+							break;
+						case 3:
+							name = "gtk-dialog-error";
+							break;
+					}
+					img.Pixbuf = Stetic.IconLoader.LoadIcon(this, name, IconSize.Menu);
+					button.Image = img;
+					buttonBox.PackStart(button, false, false, 0);
+				}
+				
+				ItemListTable.Attach(buttonBox, 10, 11, i, i+1, AttachOptions.Shrink, AttachOptions.Shrink, 0 ,0);
+                
 				if(item.Visit != null) { //Пока не даём создавать из программы новые
 					textView = new TextView(); //Коментарий
 					textView.Buffer.Text = item.Comment;
 					textView.FocusOutEvent += (sender, args) => ViewModel.AddComment(item, textView.Buffer.Text);
-					ItemListTable.Attach(textView, 14, 15, i, i+j+1, AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+					ItemListTable.Attach(textView, 12, 15, i, i+j+1, AttachOptions.Fill, AttachOptions.Fill, 0, 0);
 				}
 
 				ItemListTable.Attach(new HSeparator(), 0, 15, i+j+1, i+j+2);
