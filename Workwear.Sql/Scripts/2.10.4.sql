cff50608ce16dc362c3898c5a3346d2fdfa3ee86
Gankov <gav@qsolution.ru>
2025-10-15 16:46:27 +0300

    Перенес код для рфидов в 2.10.4

--- Workwear.Sql/Scripts/2.10.4.sql
+++ Workwear.Sql/Scripts/2.10.4.sql
@@ -182,3 +182,24 @@ ALTER TABLE stock_collective_expense
 		ON DELETE NO ACTION 
 		ON UPDATE CASCADE,
 	ADD INDEX `issuance_request_id_idx` (`issuance_request_id` ASC);
+
+-- -----------------------------------
+-- Учёт RFID
+-- -----------------------------------
+DELETE FROM barcodes WHERE title IN NULL;
+
+alter table barcodes
+	modify title varchar(24) not null;
+
+alter table barcodes
+	add type enum ('EAN13', 'EPC96') default 'EAN13' not null after last_update;
+
+create index barcodes_type_idx
+	on barcodes (type);
+
+alter table barcodes
+drop key value_UNIQUE;
+
+alter table barcodes
+	add constraint value_UNIQUE
+		unique (type, title);
