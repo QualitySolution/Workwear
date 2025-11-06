72770f05c5b1e4cd698a14dc2c31002dcad36dfd
Gankov <gav@qsolution.ru>
2025-11-06 16:52:47 +0300

    Исправлена работа на Mysql
    Там нельзя напрямую использовать изменяемую в запросе базу.

--- Workwear.Sql/Scripts/2.10.4.sql
+++ Workwear.Sql/Scripts/2.10.4.sql
@@ -244,7 +244,7 @@ ALTER TABLE stock_collective_expense
 
 -- Заполнение даты начала использования для выдач
 UPDATE operation_issued_by_employee op1 SET StartOfUse =
-	(SELECT DATE(operation_time) FROM operation_issued_by_employee op2
+	(SELECT DATE(operation_time) FROM (SELECT * FROM operation_issued_by_employee) op2
 WHERE op1.id = op2.id)
 WHERE StartOfUse IS NULL AND (issued != 0 AND returned = 0);
 
