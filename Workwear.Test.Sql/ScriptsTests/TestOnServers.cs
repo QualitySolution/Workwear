c3bab43a3b95f34a1dda56d62878011dcf878137
Gankov <gav@qsolution.ru>
2025-11-06 12:43:16 +0300

    Добавил так же тесты на mysql

--- Workwear.Test.Sql/ScriptsTests/TestOnServers.cs
+++ Workwear.Test.Sql/ScriptsTests/TestOnServers.cs
@@ -1,4 +1,5 @@
 ﻿namespace Workwear.Test.Sql.ScriptsTests {
+	#region MariaDB
 	public class MariaDb_10_6 : MariaDBUpdatesTestsBase {
 		public MariaDb_10_6() : base("mariadb:10.6") {
 		}
@@ -18,4 +19,22 @@
 		public MariaDb_latest() : base("mariadb:latest") {
 		}
 	}
+	#endregion
+
+	#region MySQL
+	public class Mysql_8_0_24 : MysqlUpdatesTestsBase {
+		public Mysql_8_0_24() : base("mysql:8.0.24") {
+		}
+	}
+	
+	public class Mysql_8_latest : MysqlUpdatesTestsBase {
+		public Mysql_8_latest() : base("mysql:8") {
+		}
+	}
+	
+	public class Mysql_latest : MysqlUpdatesTestsBase {
+		public Mysql_latest() : base("mysql:latest") {
+		}
+	}
+	#endregion
 }
