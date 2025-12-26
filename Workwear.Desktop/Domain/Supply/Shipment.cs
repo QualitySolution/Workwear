8704c446a796ada0c85d873e13d6f8cdd56c4c40
Kate <ekaterinatimireva1508@gmail.com>
2025-12-26 18:29:26 +0300

    Добавление периода при добавлении строк

--- Workwear.Desktop/Domain/Supply/Shipment.cs
+++ Workwear.Desktop/Domain/Supply/Shipment.cs
@@ -158,6 +158,8 @@ namespace Workwear.Domain.Supply {
 				Requested = 1,
 				Nomenclature = nomenclature,
 				Cost = nomenclature.SaleCost ?? 0m,
+				StartPeriod = this.StartPeriod,
+				EndPeriod = this.EndPeriod,
 			};
 			Items.Add(newItem);
 			return newItem;
