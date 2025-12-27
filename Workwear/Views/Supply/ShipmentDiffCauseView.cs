a3ee4eb1b69939b2fe49b6b9dd18041af64050f2
Kate <ekaterinatimireva1508@gmail.com>
2025-12-27 16:05:07 +0300

    Добавление вьюшки в качестве окна для заполнения причины расхождения

---
+++ Workwear/Views/Supply/ShipmentDiffCauseView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Workwear.Views.Supply {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ShipmentDiffCauseView : Gtk.Bin {
+		public ShipmentDiffCauseView() {
+			this.Build();
+		}
+		
+		protected void OnButtonFillDiffCauseClicked(object sender, EventArgs e) {
+		}
+	}
+}
