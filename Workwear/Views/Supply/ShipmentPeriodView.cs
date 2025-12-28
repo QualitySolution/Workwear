b82e25256ef345638c9a345971c6edc9bf0ebeb8
Kate <ekaterinatimireva1508@gmail.com>
2025-12-28 17:01:58 +0300

    Добавление вьюшки для заполнения периода

---
+++ Workwear/Views/Supply/ShipmentPeriodView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Workwear.Views.Supply {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ShipmentPeriodView : Gtk.Bin {
+		public ShipmentPeriodView() {
+			this.Build();
+		}
+
+		protected void OnButtonSetPeriodClicked(object sender, EventArgs e) {
+		}
+	}
+}
