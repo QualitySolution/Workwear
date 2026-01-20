6886bde8c813f2831fe8961a34e4ce9edb5be63a
Иван <space@qsolution.ru>
2026-01-20 18:07:40 +0300

    К предыдущему

--- Workwear.Desktop/Domain/Regulations/ProtectionTools.cs
+++ Workwear.Desktop/Domain/Regulations/ProtectionTools.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using QS.DomainModel.Entity;
 using QS.Extensions.Observable.Collections.List;
@@ -135,26 +136,27 @@ namespace Workwear.Domain.Regulations
 
 		#region Номенклатура
 
-		private IObservableList<Nomenclature> nomenclatures = new ObservableList<Nomenclature>();
+		private IObservableList<ProtectionToolsNomenclature> protectionToolsNomenclatures = new ObservableList<ProtectionToolsNomenclature>();
+		[Display(Name = "Связи с номенклатурами")]
+        public virtual IObservableList<ProtectionToolsNomenclature> ProtectionToolsNomenclatures {
+        	get { return protectionToolsNomenclatures; }
+	        set { SetField(ref protectionToolsNomenclatures, value); }
+        }
 
 		[Display(Name = "Номенклатура")]
-		public virtual IObservableList<Nomenclature> Nomenclatures {
-			get { return nomenclatures; }
-			set { SetField(ref nomenclatures, value, () => Nomenclatures); }
-		}
+		public virtual IEnumerable<Nomenclature> Nomenclatures => ProtectionToolsNomenclatures.Select(x => x.Nomenclature); 
 
-		public virtual void AddNomenclature(Nomenclature nomenclature)
-		{
+		public virtual void AddNomenclature(Nomenclature nomenclature) {
+			bool a = Nomenclatures.Any(x => x.Id != 0);
 			if(Nomenclatures.Any(p => DomainHelper.EqualDomainObjects(p, nomenclature))) {
 				logger.Warn("Номеклатура уже добавлена. Пропускаем...");
 				return;
 			}
-			Nomenclatures.Add(nomenclature);
+			ProtectionToolsNomenclatures.Add(new ProtectionToolsNomenclature() {ProtectionTools = this, Nomenclature = nomenclature});
 		}
 
-		public virtual void RemoveNomenclature(Nomenclature nomenclature)
-		{
-			Nomenclatures.Remove(nomenclature);
+		public virtual void RemoveNomenclature(Nomenclature nomenclature) {
+			ProtectionToolsNomenclatures.RemoveAll(x => DomainHelper.EqualDomainObjects(x.Nomenclature, nomenclature));
 		}
 		#endregion
 	}
