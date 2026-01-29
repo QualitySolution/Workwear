2f7ae2557ab602c89abfe21b1e6a9195b89b107c
Kate <ekaterinatimireva1508@gmail.com>
2026-01-29 17:32:28 +0300

    Фильтр журнала

---
+++ Workwear/Journal/Filter.Views/Regulations/DutyNormFilterView.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Workwear.Journal.Filter.Views.Regulations {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DutyNormFilterView : Gtk.Bin {
+		public DutyNormFilterView() {
+			this.Build();
+		}
+	}
+}
