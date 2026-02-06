674191417c699c5f8610899cf9701f7e9da9e825
Gankov <gav@qsolution.ru>
2026-02-06 15:40:08 +0300

    Исправлено расхождение в базах

--- Workwear.Sql/Scripts/2.10.7.sql
+++ Workwear.Sql/Scripts/2.10.7.sql
@@ -3,6 +3,6 @@ ALTER TABLE protection_tools_nomenclature
 DROP PRIMARY KEY,
     ADD COLUMN id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST,
     ADD PRIMARY KEY (id),
-    ADD UNIQUE INDEX uk_protection_tools_nomenclature (protection_tools_id, nomenclature_id);
+    ADD UNIQUE INDEX `unique_protection_tools_nomenclature` (protection_tools_id, nomenclature_id);
 -- Добавление архивирования дежурной нормы
 ALTER TABLE duty_norms ADD COLUMN archival TINYINT(1) NOT NULL DEFAULT 0 AFTER dateto;
