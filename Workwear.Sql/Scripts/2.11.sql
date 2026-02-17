b46fb9255405856fc37342a25dd42145d0ee9722
Kate <ekaterinatimireva1508@gmail.com>
2026-02-17 10:17:27 +0300

    Удаление смены в скрипте 2.11

--- Workwear.Sql/Scripts/2.11.sql
+++ Workwear.Sql/Scripts/2.11.sql
@@ -3,3 +3,6 @@ DROP FUNCTION IF EXISTS count_issue;
 
 -- Удаление таблицы work_days, которая больше не используется
 DROP TABLE work_days;
+
+-- Удаление из нормы типа периода Смена
+ALTER TABLE norms_item CHANGE period_type period_type ENUM ('Year', 'Month', 'Wearout', 'Duty');
