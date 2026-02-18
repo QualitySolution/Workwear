8b970ae18db2fc2fc6411de6c127d90e91bd1fd8
Kate <ekaterinatimireva1508@gmail.com>
2026-02-18 12:20:24 +0300

    Правки в скриптах для 2.11

--- Workwear.Sql/Scripts/2.10.8.sql
+++ Workwear.Sql/Scripts/2.10.8.sql
@@ -1,5 +1,2 @@
 ﻿-- Добавление комментария в подразделения
 ALTER TABLE subdivisions ADD COLUMN comment TEXT NULL DEFAULT NULL;
-
--- Замена периода Смена на Месяц в нормах
-UPDATE norms_item SET period_type = 'Month' WHERE period_type = 'Shift';
