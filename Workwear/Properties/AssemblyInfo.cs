2823605a82fa6772b68c4d2eaf4cf3f541092ac2
Gankov <gav@qsolution.ru>
2026-03-16 17:16:04 +0300

    2.10.9

--- Workwear/Properties/AssemblyInfo.cs
+++ Workwear/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using QS.Project.Versioning;
 
 [assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
 [assembly: AssemblyAuthor("Горячев Иван <space@qsolution.ru>")]
-[assembly: AssemblyAuthor("Тимирева Екатерина <e.timireva@qsolution.ru>")]
+[assembly: AssemblyAuthor("Тимирева Екатерина 2024-2025")]
 [assembly: AssemblyAuthor("Покровский Михаил 2024")]
 [assembly: AssemblyAuthor("Мингалимов Ильяс 2022")]
 [assembly: AssemblyAuthor("Солдаткина Алина 2020")]
@@ -32,7 +32,7 @@ using QS.Project.Versioning;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("2.10.8")]
+[assembly: AssemblyVersion("2.10.9")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
