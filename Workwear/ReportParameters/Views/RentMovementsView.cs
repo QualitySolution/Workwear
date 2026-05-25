d204abb654b7599af3c8b68b72b9f94b993b5621
Иван <space@qsolution.ru>
2026-05-25 09:14:02 +0300

    Добавил в интерфейс

---
+++ Workwear/ReportParameters/Views/RentMovementsView.cs
@@ -0,0 +1,18 @@
+﻿using QS.Views;
+using Workwear.ReportParameters.ViewModels;
+
+namespace Workwear.ReportParameters.Views {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RentMovementsView : ViewBase<RentMovementsViewModel> {
+		public RentMovementsView(RentMovementsViewModel viewModel) : base(viewModel) {
+			this.Build();
+			
+			ydateperiodpicker.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.StartDate, w => w.StartDateOrNull)
+				.AddBinding(vm => vm.EndDate, w => w.EndDateOrNull)
+				.InitializeFromSource();
+			choiceNomenclatures.ViewModel = ViewModel.ChoiceNomenclatureViewModel;
+			buttonRun.Clicked += (sender, args) => ViewModel.LoadReport();
+		}
+	}
+}
