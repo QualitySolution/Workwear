e99b888b041362d1fa48c04831088f92a899897c
Gankov <gav@qsolution.ru>
2026-07-24 12:46:33 +0300

    Переделал журнал на использование хебернейта.

--- Workwear/Journal/ViewModels/Stock/OverNormIssuedJournalViewModel.cs
+++ Workwear/Journal/ViewModels/Stock/OverNormIssuedJournalViewModel.cs
@@ -2,31 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using Dapper;
 using Gamma.Utilities;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Dialect.Function;
+using NHibernate.SqlCommand;
+using NHibernate.Transform;
 using QS.Dialog;
 using QS.DomainModel.UoW;
 using QS.Navigation;
-using QS.Project.DB;
 using QS.Project.Journal;
 using QS.Project.Journal.DataLoader;
-using QS.Project.Journal.Search;
 using QS.ViewModels.Extension;
 using Workwear.Domain.Company;
 using Workwear.Domain.Operations;
+using Workwear.Domain.Sizes;
+using Workwear.Domain.Stock;
 using Workwear.Tools;
 
 namespace Workwear.Journal.ViewModels.Stock {
 	public class OverNormIssuedJournalViewModel : JournalViewModelBase, IDialogDocumentation {
-		private readonly IConnectionFactory connectionFactory;
-
 		public OverNormIssuedJournalViewModel(
 			IUnitOfWorkFactory unitOfWorkFactory,
 			IInteractiveService interactiveService,
-			INavigationManager navigation,
-			IConnectionFactory connectionFactory
+			INavigationManager navigation
 		) : base(unitOfWorkFactory, interactiveService, navigation) {
-			this.connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
 			DataLoader = new AnyDataLoader<OverNormIssuedJournalNode>(GetNodes);
 			TableSelectionMode = JournalSelectionMode.Multiple;
 			Title = "Выдано сверх нормы";
@@ -48,68 +48,84 @@ namespace Workwear.Journal.ViewModels.Stock {
 		}
 
 		private IList<OverNormIssuedJournalNode> GetNodes(CancellationToken cancellation) {
-			using(var connection = connectionFactory.OpenConnection()) {
-				var conditions = new List<string> {
-					"warehouse_operation.warehouse_expense_id IS NOT NULL",
-					"operation_over_norm.type <> 'Simple'",
-					"operation_over_norm.return_from_operation IS NULL",
-					@"NOT EXISTS (
-						SELECT 1
-						FROM operation_over_norm returned_operation
-						WHERE returned_operation.return_from_operation = operation_over_norm.id
-					)"
-				};
+			using(var uow = UnitOfWorkFactory.CreateWithoutRoot()) {
+				OverNormIssuedJournalNode resultAlias = null;
+				OverNormOperation operationAlias = null;
+				OverNormOperation returnedOperationAlias = null;
+				WarehouseOperation warehouseOperationAlias = null;
+				EmployeeCard employeeAlias = null;
+				Nomenclature nomenclatureAlias = null;
+				Size sizeAlias = null;
+				Size heightAlias = null;
+				BarcodeOperation barcodeOperationAlias = null;
+				Barcode barcodeAlias = null;
+
+				var returnedOperationSubquery = QueryOver.Of(() => returnedOperationAlias)
+					.Where(() => returnedOperationAlias.ReturnFromOperation.Id == operationAlias.Id)
+					.Select(x => x.Id)
+					.Take(1);
+
+				var employeeNameProjection = Projections.SqlFunction(
+					new SQLFunctionTemplate(NHibernateUtil.String, "CONCAT_WS(' ', ?1, ?2, ?3)"),
+					NHibernateUtil.String,
+					Projections.Property(() => employeeAlias.LastName),
+					Projections.Property(() => employeeAlias.FirstName),
+					Projections.Property(() => employeeAlias.Patronymic));
+
+				var barcodesProjection = Projections.SqlFunction(
+					new SQLFunctionTemplate(NHibernateUtil.String, "GROUP_CONCAT(DISTINCT ?1 SEPARATOR '\n')"),
+					NHibernateUtil.String,
+					Projections.Property(() => barcodeAlias.Title));
+
+				var query = uow.Session.QueryOver(() => operationAlias)
+					.JoinAlias(() => operationAlias.WarehouseOperation, () => warehouseOperationAlias)
+					.JoinAlias(() => operationAlias.Employee, () => employeeAlias)
+					.JoinAlias(() => operationAlias.Nomenclature, () => nomenclatureAlias)
+					.Left.JoinAlias(() => operationAlias.WearSize, () => sizeAlias)
+					.Left.JoinAlias(() => operationAlias.Height, () => heightAlias)
+					.Left.JoinAlias(() => operationAlias.BarcodeOperations, () => barcodeOperationAlias)
+					.Left.JoinAlias(() => barcodeOperationAlias.Barcode, () => barcodeAlias)
+					.Where(() => warehouseOperationAlias.ExpenseWarehouse != null)
+					.Where(() => operationAlias.Type != OverNormType.Simple)
+					.Where(() => operationAlias.ReturnFromOperation == null)
+					.WithSubquery.WhereNotExists(returnedOperationSubquery)
+					.Where(GetSearchCriterion(
+						() => employeeAlias.LastName,
+						() => employeeAlias.FirstName,
+						() => employeeAlias.Patronymic,
+						() => employeeAlias.PersonnelNumber,
+						() => nomenclatureAlias.Name,
+						() => barcodeAlias.Title
+					));
 
 				if(Employee != null)
-					conditions.Add("operation_over_norm.employee_id = @employee_id");
+					query.Where(() => employeeAlias.Id == Employee.Id);
 
-				var search = new SqlSearchCriterion(Search)
-					.WithLikeMode(LikeMatchMode.StringAnywhere)
-					.By("employees.last_name")
-					.By("employees.first_name")
-					.By("employees.patronymic")
-					.By("employees.personnel_number")
-					.By("nomenclature.name")
-					.By("barcode.title")
-					.Finish();
-				if(!String.IsNullOrEmpty(search))
-					conditions.Add(search);
-
-				var sql = $@"
-SELECT
-	operation_over_norm.id AS Id,
-	operation_over_norm.type AS Type,
-	CONCAT_WS(' ', employees.last_name, employees.first_name, employees.patronymic) AS EmployeeName,
-	employees.personnel_number AS PersonnelNumber,
-	nomenclature.name AS NomenclatureName,
-	sizealias.name AS WearSize,
-	heightalias.name AS Height,
-	warehouse_operation.amount AS Amount,
-	warehouse_operation.wear_percent AS WearPercent,
-	warehouse_operation.operation_time AS Date,
-	GROUP_CONCAT(DISTINCT barcode.title SEPARATOR '\n') AS Barcodes
-FROM operation_over_norm
-	JOIN operation_warehouse warehouse_operation ON warehouse_operation.id = operation_over_norm.operation_warehouse_id
-	JOIN employees ON employees.id = operation_over_norm.employee_id
-	JOIN nomenclature ON nomenclature.id = operation_over_norm.nomenclature_id
-	LEFT JOIN sizes sizealias ON sizealias.id = operation_over_norm.size_id
-	LEFT JOIN sizes heightalias ON heightalias.id = operation_over_norm.height_id
-	LEFT JOIN operation_barcodes barcode_operation ON barcode_operation.over_norm_operation_id = operation_over_norm.id
-	LEFT JOIN barcodes barcode ON barcode.id = barcode_operation.barcode_id
-WHERE {String.Join(" AND ", conditions)}
-GROUP BY operation_over_norm.id
-ORDER BY warehouse_operation.operation_time DESC, operation_over_norm.id DESC";
-
-				return connection.Query<OverNormIssuedJournalNode>(sql, new {
-					employee_id = Employee?.Id
-				}).ToList();
+				return query
+					.SelectList(list => list
+						.SelectGroup(() => operationAlias.Id).WithAlias(() => resultAlias.Id)
+						.Select(() => operationAlias.Type).WithAlias(() => resultAlias.Type)
+						.Select(employeeNameProjection).WithAlias(() => resultAlias.EmployeeName)
+						.Select(() => employeeAlias.PersonnelNumber).WithAlias(() => resultAlias.PersonnelNumber)
+						.Select(() => nomenclatureAlias.Name).WithAlias(() => resultAlias.NomenclatureName)
+						.Select(() => sizeAlias.Name).WithAlias(() => resultAlias.WearSize)
+						.Select(() => heightAlias.Name).WithAlias(() => resultAlias.Height)
+						.Select(() => warehouseOperationAlias.Amount).WithAlias(() => resultAlias.Amount)
+						.Select(() => warehouseOperationAlias.WearPercent).WithAlias(() => resultAlias.WearPercent)
+						.Select(() => warehouseOperationAlias.OperationTime).WithAlias(() => resultAlias.Date)
+						.Select(barcodesProjection).WithAlias(() => resultAlias.Barcodes)
+					)
+					.OrderBy(() => warehouseOperationAlias.OperationTime).Desc
+					.ThenBy(() => operationAlias.Id).Desc
+					.TransformUsing(Transformers.AliasToBean<OverNormIssuedJournalNode>())
+					.List<OverNormIssuedJournalNode>();
 			}
 		}
 	}
 
 	public class OverNormIssuedJournalNode {
 		public int Id { get; set; }
-		public string Type { get; set; }
+		public OverNormType Type { get; set; }
 		public string EmployeeName { get; set; }
 		public string PersonnelNumber { get; set; }
 		public string NomenclatureName { get; set; }
@@ -119,7 +135,7 @@ ORDER BY warehouse_operation.operation_time DESC, operation_over_norm.id DESC";
 		public decimal WearPercent { get; set; }
 		public DateTime Date { get; set; }
 		public string Barcodes { get; set; }
-		public string TypeText => ((OverNormType)Enum.Parse(typeof(OverNormType), Type)).GetEnumTitle();
+		public string TypeText => Type.GetEnumTitle();
 		public string DateText => Date.ToShortDateString();
 		public string WearPercentText => WearPercent.ToString("P0");
 	}
