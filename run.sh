860fa10958dbdcf01b85f7c7da725d5d7e3eef93
Gankov <gav@qsolution.ru>
2026-08-19 14:56:03 +0300

    Исправлен запуск из консоли на опен суси.

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd "$(dirname "$0")/Workwear/bin/Debug"
-mono workwear.exe
+cd "$(dirname "$0")/Workwear/bin/Debug" || exit 1
+
+# Mono 6.12 cannot read modern terminfo entries larger than 4 KiB.
+# openSUSE's xterm-256color entry is larger and breaks System.Console.
+export TERM=screen-256color
+
+exec mono workwear.exe "$@"
