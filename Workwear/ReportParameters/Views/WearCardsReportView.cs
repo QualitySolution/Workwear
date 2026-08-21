20e7f2d1b08d28816a8d6116392220b10f179b9d
Иван <space@qsolution.ru>
2026-08-21 18:09:01 +0300

    Модификация отчёта  "Списки сотрудников"

--- Workwear/ReportParameters/Views/WearCardsReportView.cs
+++ Workwear/ReportParameters/Views/WearCardsReportView.cs
@@ -6,8 +6,26 @@ namespace Workwear.ReportParameters.Views {
 	public partial class WearCardsReportView : ViewBase<WearCardsReportViewModel> {
 		public WearCardsReportView(WearCardsReportViewModel viewModel) : base(viewModel) {
 			this.Build();
-			ycheckbuttonOnlyWithoutNorms.Binding.AddBinding(ViewModel, v=>v.OnlyWithoutNorms, w=>w.Active).InitializeFromSource();
+
 			ycheckbuttonOnlyWorking.Binding.AddBinding(ViewModel, v=>v.OnlyWorking, w=>w.Active).InitializeFromSource();
+			ydateActualEmployee.Binding.AddBinding(ViewModel, v=>v.ActualDate, w=>w.Date).InitializeFromSource();
+
+			ycheckbuttonHire.Binding.AddBinding(ViewModel, v=>v.OnlyHired, w=>w.Active).InitializeFromSource();
+			ydateperiodpicker_.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.HirePeriodStart, w => w.StartDate)
+				.AddBinding(vm => vm.HirePeriodEnd, w => w.EndDate)
+				.InitializeFromSource();
+
+			ycheckbuttonDismiss.Binding.AddBinding(ViewModel, v=>v.OnlyDismissed, w=>w.Active).InitializeFromSource();
+			ydateperiodpicker_dismiss.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.DismissPeriodStart, w => w.StartDate)
+				.AddBinding(vm => vm.DismissPeriodEnd, w => w.EndDate)
+				.InitializeFromSource();
+
+			ycheckbuttonOnlyWithoutNorms.Binding.AddBinding(ViewModel, v=>v.OnlyWithoutNorms, w=>w.Active).InitializeFromSource();
+			ycheckbuttonOnlyWithNorms.Binding.AddBinding(ViewModel, v=>v.OnlyWithNorms, w=>w.Active).InitializeFromSource();
+
+			ycheckbuttonShowPhone.Binding.AddBinding(ViewModel, v=>v.ShowPhone, w=>w.Active).InitializeFromSource();
 		}
 		protected void OnYbuttonRunClicked(object sender, EventArgs e) {
 			ViewModel.LoadReport();
